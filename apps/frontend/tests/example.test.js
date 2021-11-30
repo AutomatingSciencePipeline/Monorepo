714c0db933e3a89361ff974f31ae30dcc2e87233
Elijah Williams <williae2@rose-hulman.edu>
2021-11-30 18:53:51 -0500

    begun looking into jest for unit testing javascript

---
+++ apps/frontend/tests/example.test.js
@@ -0,0 +1,12 @@
+test('THIS IS A TEST: two plus two is four', () => {
+    expect(2 + 2).toBe(4);
+});
+test('THIS IS A TEST: two plus two is five?', () => {
+    expect(2 + 2).toBe(5);
+});
+test('THIS IS A TEST: 3 plus 312 is 315', () => {
+    expect(3 + 312).toBe(315);
+});
+test('THIS IS A TEST: 1 plus -13 is -12', () => {
+    expect(1 + -13).toBe(-12);
+});
