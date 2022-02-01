bbc694ad3cdcaf210b77c1a87e223722a814e462
rhit-fayoumoa <fayoumoa@rose-hulman.edu>
2022-02-01 09:37:01 -0500

    init prod

---
+++ apps/backend/balance.py
@@ -0,0 +1,241 @@
+import sys, os
+import re
+from subprocess import Popen, PIPE, STDOUT
+import select
+import logging
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor, as_completed
+import importlib
+import requests
+from flask import Flask, jsonify, request
+import numpy as np
+import collections
+import itertools
+import csv
+import copy
+import json
+import argparse
+import stat
+
+# parser = argparse.ArgumentParser(description="Initialize GLADOS Global Load Balancer.")
+# parser.add_argument('--N', dest='nworkers',type=int,help='Number of thread / process workers to use.')
+
+app = Flask(__name__)
+app.config['DEBUG'] = False
+
+### FLASK API ENDPOINTS
+
+@app.post("/experiment")
+def recv_experiment():
+    exp = request.get_json()
+    exp = proc_msg(exp)
+    GlobalLoadBalancer.submit_experiment(exp)
+    return 'OK'
+
+### GLB
+class GLB(object):
+
+    def __init__(self, n_workers=None):
+        self.e_status = {}
+        self.e = ProcessPoolExecutor(n_workers)
+        self.f = ThreadPoolExecutor(n_workers)
+        logging.debug(f'GLB: {n_workers} workers initialized. Awaiting requests...')
+        
+    def submit_experiment(self, msg):
+        self.e_status[msg['id']] = 'INIT'
+        msg['func'] = add_nums
+        logging.info(f'Experiment {msg["id"]} by {msg["user"]} submitted.')
+        self.e.submit(experiment_event, msg).add_done_callback(experiment_resolve)
+        
+    
+    def update_experiment_status(self, id, status):
+        self.e_status[id] = status
+        
+
+def experiment_event(msg):
+    params = msg
+    # within each experiment, we use threads
+    # if not os.path.exists(params['fileName']):
+    #     raise
+    os.chmod(f'incoming/{params["fileName"]}', 0o777)
+    os.mkdir(f'exps/{params["id"]}')
+    os.rename(f'incoming/{params["fileName"]}', f'exps/{params["id"]}/{params["fileName"]}')
+    os.chdir(f'exps/{params["id"]}')
+    param_iter = gen_configs(params['parameters'])
+    ## we submit experiment configuration writing to a thread pool if verbose
+    if params['verbose']:
+        os.mkdir('configs')
+        with ThreadPoolExecutor(1) as e:
+            e.submit(write_configs, param_iter, [obs['paramName'] for obs in params['parameters']])
+    
+    func = params['func']
+    ## process stuff and make API call to inform of the experiment's commencement
+    results = []
+    dead = 0
+    i = 0
+    with ThreadPoolExecutor(1) as executor:
+        result_futures = list(map(lambda x: executor.submit(mapper, {'filename': params['fileName'],'iter':x[0],'func':func,'params':x[1]}), list(param_iter)))
+        for future in as_completed(result_futures):
+            try:
+                res = future.result()
+                results.append({'iter': res[0], 'params': res[1], 'result': res[2]})
+                if i % 10 == 0:
+                    logging.info(f'{i} iterations completed.')
+            except Exception as e:
+                ### write temporary state of problems
+                print(e)
+                results.append({'iter': 0, 'params': [0], 'result': 'FAILED'})
+                dead+=1
+            i+=1
+    
+    ## process stuff and make API call to inform of the experiment's completion
+    ### write results to a csv file named experiment_id.csv
+    with open(f'result.csv', 'w') as csvfile:
+        header = ['iter']
+        header += [k['paramName'] for k in params['parameters']]
+        header.append('result')
+        writer = csv.writer(csvfile)
+        writer.writerow(header)
+        # print(results)
+        writer.writerows(list(map(lambda x: post(x), results)))
+
+    
+    return params['id'],1.-float(dead)/len(results),results
+
+def post(s):
+    s = list(s.values())
+    s = [s[0]] + s[1] + [s[2]]
+    return s
+
+def experiment_resolve(future):
+    ### Make API call to inform of completion of experiment
+    # if future.exception():
+        
+    id, prog, results = future.result()
+    GlobalLoadBalancer.update_experiment_status(id, 'DONE')
+    logging.info(f'[EXP COMPLETE]:\tExperiment {id} completed with {prog} success rate.')
+
+def mapper(params):
+    #try: 
+    #   return params['iter'], list(params['params']), [params['func'](*params['params'])]
+    #except Exception as e:
+    #   raise Exception(f'Mapper failed with exception: {e} for the iteration with params', params['iter'], params['params'])
+    try:
+        #print(params['params'])
+        result = communicate(f'./{params["filename"]}', list(map(str, list(params['params']))))
+    except Exception as e:
+        raise Exception(f'Mapper failed with exception: {e} for the')
+    return params['iter'], list(params['params']), result
+    
+    
+
+### UTILS
+
+def gen_configs(hyperparams):
+    ### Generate hyperparameter configurations
+    params_raw = [k['values'] for k in hyperparams]
+    params_raw = [[x for x in np.arange(k[0],k[1]+k[2],k[2])] for k in params_raw]
+    return enumerate(list(itertools.product(*params_raw)))
+
+def write_configs(raw, headers):
+    dicts = [{headers[i]:np_uncode(x[i]) for i in range(len(x))} for _,x in copy.deepcopy(raw)]
+    jsons = [json.dumps(x) for x in dicts]
+    for i,_ in copy.deepcopy(raw):
+        with open(f'configs/config_{i}.json', 'w+') as f:
+            f.write(jsons[i])
+        
+
+def proc_msg(msg):
+    ## for now, this function is hardcoding some things, but it's no biggie
+    rm = copy.deepcopy(msg)
+    for obj in rm['parameters']:
+        obj['values'] = [float(x) for x in obj['values']]
+        obj['values'] = [x for i,x in enumerate(obj['values']) if i > 0]
+
+    rm['func'] = add_nums
+    rm['id'] = rm['experimentName']
+    del rm['experimentName']
+    return rm
+
+def np_uncode(x):
+    if isinstance(x,np.integer):
+        return int(x)
+    if isinstance(x,np.floating):
+        return float(x)
+
+
+
+def flatten(x):
+    if isinstance(x, collections.Iterable):
+        return [a for i in x for a in flatten(i)]
+    else:
+        return [x]
+
+def initilize_work_space():
+    names = ['exps','res','incoming']
+    if not os.path.exists('GLADOS_HOME'):
+        os.mkdir('GLADOS_HOME')
+    for name in names:
+        if not os.path.exists(f'GLADOS_HOME/{name}'):
+            os.mkdir(f'GLADOS_HOME/{name}')
+    os.chdir('GLADOS_HOME')
+
+
+def communicate(process, payload):
+    with Popen([process] + payload, stdout=PIPE, stdin=PIPE, stderr=PIPE,encoding='utf8') as p:
+        try:
+            stdout_data = p.communicate()
+            #if not stdout_data:
+            #    raise UnresponsiveBinaryException(f'{process} is unresponsive.')
+            if stdout_data[1]:
+                print(f'errors returned from pipe is {stdout_data[1]}')
+                raise FailedIterationException(f'Iteration has failed with error {stdout_data[1]}')
+        except Exception as e:
+            raise PipeFailureException(f'Error communicating with process: {e}')
+    return float(stdout_data[0])
+
+### TYPES 
+
+class FailedIterationException(Exception):
+    pass
+
+class PipeFailureException(Exception):
+    pass
+
+class UnresponsiveBinaryException(Exception):
+    pass
+
+### EXPERIMENTAL ARTIFACTS
+
+def add_nums(x,y):
+    ## for testing purposes.
+    return x+y
+
+if __name__=='__main__':
+    logging.getLogger().setLevel(logging.DEBUG)
+    initilize_work_space()
+    GlobalLoadBalancer = GLB(1)
+    hyperparams = [{'paramName':'x','values':[0,10,0.1]},{'paramName':'y','values':[5,100,5]}]
+    msg_test = {
+        'id' : 'XVZ01',
+        'user' : 'elijah',
+        'func' : add_nums,
+        'hyperparams' : hyperparams
+    }
+    # GlobalLoadBalancer.submit_experiment(msg_test)
+    app.run()
+    #blablabla = gen_configs(msg_test['hyperparams'])
+    #write_configs(blablabla, [obs['paramName'] for obs in msg_test['hyperparams']])
+    #communicate('./add_nums.py', ['1','2'])
+    
+'''
+### plot postprocessing / native support ###
+### gen intermediate plots live ###
+### [debug mode]: switch to not write configuration
+### intermediate checks: every n iterations, restrict param space based on provided code
+### MPI
+
+1. refined front-end >> integrate with the db and crud 
+2.  > modularity:
+    > distribution across the network >> MPI 
+3. stitch the system together
+'''
---
+++ services/migrations/db_modules.py
@@ -0,0 +1,176 @@
+from pymongo import MongoClient
+from pprint import pprint
+import mariadb
+import sys
+
+def CreateExperiment(Name, Status, User, newID):
+    try:
+        conn = mariadb.connect(
+        user = "glados",
+        password = "GladosPass",
+        host = "localhost",
+        port = 3306
+        )
+        curs = conn.cursor()
+        curs.execute("use demo;")
+        statement = "INSERT INTO Experiment (Name, Status, User) VALUES (%s, %s, %s);"
+        data = (Name, Status, User)
+        curs.execute(statement, data)        
+        conn.commit()
+        curs.execute("SELECT ID FROM Experiment where Name = ?",(Name,))
+        for(ID) in curs:
+            return ID[0]
+    except mariadb.Error as e:
+        print(f"Error connecting to mariaDB platform: {e}")
+        sys.exit(1)
+
+
+def FindMaria(ID):
+    try:
+        conn = mariadb.connect(
+        user = "glados",
+        password = "GladosPass",
+        host = "localhost",
+        port = 3306
+        )
+        curs = conn.cursor()
+        curs.execute("use demo;")
+        curs.execute("SELECT * FROM Experiment where ID = ?",(ID,))
+        for(ID, User, Name, CreationDate, Status, NoSQLKEY) in curs:
+            return [str(ID), User, Name, str(CreationDate), Status, str(NoSQLKEY)]
+    except mariadb.Error as e:
+        print(f"Error connecting to mariaDB platform: {e}")
+        sys.exit(1)
+
+
+def UpdateKey(Key, Name):
+    try:
+        conn = mariadb.connect(
+        user = "glados",
+        password = "GladosPass",
+        host = "localhost",
+        port = 3306
+        )
+        curs = conn.cursor()
+        curs.execute("use demo;")
+        curs.execute("UPDATE Experiment  SET NoSQLKEY = ? WHERE Name = ?;",(Key,Name,))
+        conn.commit()
+    except mariadb.Error as e:
+        print(f"Error connecting to mariaDB platform: {e}")
+        sys.exit(1)
+
+
+
+def UpdateName(ID, Name):
+    try:
+        conn = mariadb.connect(
+        user = "glados",
+        password = "GladosPass",
+        host = "localhost",
+        port = 3306
+        )
+        curs = conn.cursor()
+        curs.execute("use demo;")
+        curs.execute("UPDATE Experiment  SET Name = ? WHERE ID = ?;",(Name,ID,))
+        conn.commit()
+    except mariadb.Error as e:
+        print(f"Error connecting to mariaDB platform: {e}")
+        sys.exit(1)
+
+
+
+def UpdateStatus(Status, Name):
+    try:
+        conn = mariadb.connect(
+        user = "glados",
+        password = "GladosPass",
+        host = "localhost",
+        port = 3306
+        )
+        curs = conn.cursor()
+        curs.execute("use demo;")
+        curs.execute("UPDATE Experiment  SET Status = ? WHERE Name = ?;",(Status,Name,))
+        conn.commit()
+    except mariadb.Error as e:
+        print(f"Error connecting to mariaDB platform: {e}")
+        sys.exit(1)
+
+
+def InsertResults(Name, Results, NewID):
+    client = MongoClient("mongodb://127.0.0.1:27017/?compressors=disabled&gssapiServiceName=mongodb")
+    db=client['mydb']
+    ExperimentTest = {'Name' : Name,
+        'Results' : Results}
+    return db.Experiment.insert_one(ExperimentTest).inserted_id
+    
+def RemoveExperiment(ID):
+    try:
+        conn = mariadb.connect(
+        user = "glados",
+        password = "GladosPass",
+        host = "localhost",
+        port = 3306
+        )
+        curs = conn.cursor()
+        curs.execute("use demo;")
+        curs.execute("Delete FROM Experiment  WHERE ID = ?;",(ID,))
+        conn.commit()
+    except mariadb.Error as e:
+        print(f"Error connecting to mariaDB platform: {e}")
+        sys.exit(1)
+
+def ShowAll():
+    try:
+        conn = mariadb.connect(
+        user = "glados",
+        password = "GladosPass",
+        host = "localhost",
+        port = 3306
+        )
+        curs = conn.cursor()
+        curs.execute("use demo;")
+        curs.execute("Select * from Experiment")
+        allthings = []
+        for(ID, User, Name, CreationDate, Status, NoSQLKEY) in curs:
+             allthings.append([str(ID), User, Name, str(CreationDate), Status, str(NoSQLKEY)])
+        return allthings
+    except mariadb.Error as e:
+        print(f"Error connecting to mariaDB platform: {e}")
+        sys.exit(1)
+
+
+
+def FindMongo(Name):
+    client = MongoClient("mongodb://127.0.0.1:27017/?compressors=disabled&gssapiServiceName=mongodb")
+    db=client['mydb']
+    exper = db.Experiment.find_one({'Name': Name})
+    return exper
+
+def InitDatabases():
+    client = MongoClient("mongodb://127.0.0.1:27017/?compressors=disabled&gssapiServiceName=mongodb")
+    db=client.mydb
+    try:
+        print('trying to connect with db')
+        conn = mariadb.connect(
+            user = "glados",
+            password = "GladosPass",
+            host = "localhost",
+            port = 3306
+        )
+        print('connection works')
+    except mariadb.Error as e:
+        print(f"Error connecting to mariaDB platform: {e}")
+        sys.exit(1)
+
+    cur = conn.cursor()
+    cur.execute("DROP DATABASE IF EXISTS demo;")
+    cur.execute("CREATE DATABASE demo;")
+    cur.execute("use demo;")
+    cur.execute("CREATE TABLE IF NOT EXISTS Experiment (ID int NOT NULL AUTO_INCREMENT, User varchar(255), Name varchar(255), CreationDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP, Status varchar(255), NoSQLKEY varchar(255), PRIMARY KEY(ID));")
+    cur.execute("CREATE TABLE  IF NOT EXISTS Job_Server (ID int NOT NULL, Online Boolean, CreationDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY(ID));")
+    cur.execute("CREATE TABLE IF NOT EXISTS Job (ID int NOT NULL, Experiment int, Machine int, ForeignKey int NOT NULL, CreationDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY(ID), FOREIGN KEY (Machine) REFERENCES Job_Server(ID), FOREIGN KEY (Experiment) REFERENCES Experiment(ID));")
+
+
+
+    
+    
