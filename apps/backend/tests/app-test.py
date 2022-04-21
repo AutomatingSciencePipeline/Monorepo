4869900d76afa66a44756ed1afcc24db39c1c3fa
Pavani <51791613+pavaniravella@users.noreply.github.com>
2022-04-21 13:40:56 -0400

    adding more tests trying to get docker to run

--- apps/backend/tests/app-test.py
+++ apps/backend/tests/app-test.py
@@ -1,7 +1,7 @@
 from ast import Assert
 import pytest
 import unittest
-import app
+from backend import app
 
 # def test_gen_configs(self):
 #     sample_json =  { [{ paramName: 'x', values: [1,0,4,1], type: 'integer' }, { paramName: 'y', value: [6,5,9,1], type: 'integer' }, { paramName: 'add', value: true, type: 'boolean' }] }
@@ -10,9 +10,23 @@ import app
 #     assertTrue(result, "")
 
 
-sample_json =  { [{ paramName: 'x', values: [1,0,4,1], type: 'integer' }, { paramName: 'y', value: [6,5,9,1], type: 'integer' }, { paramName: 'add', value: true, type: 'boolean' }] }
-result = app.gen_configs(sample_json)
-print(result)
+class Test_TestAppBackend(unittest.TestCase):
+    def test_gen_config(self):
+        sample_json =  { [{ paramName: 'x', values: [1,0,4,1], type: 'integer' }, { paramName: 'y', value: [6,5,9,1], type: 'integer' }, { paramName: 'add', value: true, type: 'boolean' }] }
+        result = app.gen_configs(sample_json)
+        print(result)
+        self.assertTrue(result, "")
+
+    def test_gen_config_array(self):
+        sample_json =  { [{ paramName: 'x', values: [1,0,4,1], type: 'integer' }, { paramName: 'y', value: [6,5,9,1], type: 'integer' }, { paramName: 'add', value: true, type: 'boolean' }] }
+        result = app.gen_configs(sample_json)
+        self.assertTrue(result, "")
+
+    def test_gen_config_add(self):
+        sample_json =  { [{ paramName: 'x', values: [1,0,4,1], type: 'integer' }, { paramName: 'y', value: [6,5,9,1], type: 'integer' }, { paramName: 'add', value: true, type: 'boolean' }] }
+        result = app.gen_configs(sample_json)
+        print(result)
+        self.assertTrue(result, "")
 
 if __name__ == '__main__':
     unittest.main()
