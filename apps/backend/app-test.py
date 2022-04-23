119a2c6ed0847e24663da52a9946ec0c53d52cf2
Pavani <51791613+pavaniravella@users.noreply.github.com>
2022-04-23 11:44:06 -0400

    fixed compliation errors in app-py

--- apps/backend/app-test.py
+++ apps/backend/app-test.py
@@ -6,18 +6,18 @@ import json
 
 class Test_TestAppBackend(unittest.TestCase):
     def test_gen_config(self):
-        sample_json =  { [{ paramName: 'x', values: [1,0,4,1], type: 'integer' }, { paramName: 'y', value: [6,5,9,1], type: 'integer' }, { paramName: 'add', value: true, type: 'boolean' }] }
+        sample_json =  { [{ 'paramName': 'x', 'values': [1,0,4,1], type: 'integer' }, { 'paramName': 'y', 'value': [6,5,9,1], type: 'integer' }, { 'paramName': 'add', 'value': 'true' , type: 'boolean' }] }
         result = app.gen_configs(sample_json)
         print(result)
         self.assertTrue(result, "")
 
     def test_gen_config_array(self):
-        sample_json =  { [{ paramName: 'x', values: [1,0,4,1], type: 'integer' }, { paramName: 'y', value: [6,5,9,1], type: 'integer' }, { paramName: 'add', value: true, type: 'boolean' }] }
+        sample_json =  { [{ 'paramName': 'x', 'values': [1,0,4,1], type: 'integer' }, { 'paramName': 'y', 'value': [6,5,9,1], type: 'integer' }, {"paramName": 'add', 'value' : 'true', type: 'boolean' }] }
         result = app.gen_configs(sample_json)
         self.assertTrue(result, "")
      #I have to figure out the input 
     def test_gen_config_add(self):
-        sample_json =  { [{ paramName: 'x', values: [1,0,4,1], type: 'integer' }, { paramName: 'y', value: [6,5,9,1], type: 'integer' }, { paramName: 'add', value: true, type: 'boolean' }] }
+        sample_json =  { [{ 'paramName': 'x', 'values': [1,0,4,1], type: 'integer' }, {'paramName': 'y', 'value': [6,5,9,1], type: 'integer' }, { "paramName": 'add', 'value': 'true' , type: 'boolean' }] }
         result = app.gen_configs(sample_json)
         print(result)
         self.assertTrue(result, "")
