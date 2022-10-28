d3a505c42a820e047a45cdd60a1850995951b3fc
Robb <computerguy440+gh@gmail.com>
2022-10-28 00:21:05 -0400

    Remove no longer used supabase content

--- apps/frontend/pages/api/auth.js
+++
@@ -1,7 +0,0 @@
-import supabase from '../../supabase/client';
-
-const handler = (req, res) => {
-	supabase.auth.api.setAuthCookie(req, res);
-};
-
-export default handler;
--- apps/frontend/supabase/client.js
+++
@@ -1,9 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabase = createClient(
-	process.env.NEXT_PUBLIC_SUPABASE_URL,
-	process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-);
-
-// export const auth = supabase.auth;
-export default supabase;
