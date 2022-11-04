5ed5444d76006870595df9b95185163c106a258f
Robb <computerguy440+gh@gmail.com>
2022-11-04 01:53:49 -0400

    Swarm exit info

--- dev-docker-host-setup.sh
+++ dev-docker-host-setup.sh
@@ -5,5 +5,9 @@
 # Init this machine as the swarm manager -> it can't join a swarm
 docker swarm init
 
+# TODO we need to export what this returns somewhere for other things to connect to
+
 # create overlay network called glados-overlay that non-swarm hosts are capable of joining
 docker network create -d overlay glados-overlay --attachable
+
+# TODO info on exiting the swarm via `docker swarm leave --force`
