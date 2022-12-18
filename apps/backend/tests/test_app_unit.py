b559d6eaf80e0d716c54053aa4dfa80d7a9bb540
robinsr2 <robinsr2@rose-hulman.edu>
2022-12-18 14:58:20 -0500

    remove unneeded code

--- apps/backend/tests/test_app_unit.py
+++ apps/backend/tests/test_app_unit.py
@@ -21,65 +21,6 @@ class TestBackend(unittest.TestCase):
         expected = [(0, (0, 6)), (1, (1, 6)), (2, (1, 5)), (3, (1, 6))]
         print(result)
         self.assertEqual(result, expected)
-        
-    def test_concat_array_empty(self):
-        array = []
-        array2 = [1, 2, 3]
-        array= app.concat_arrays(array, array2)
-        expected =[1, 2, 3]
-        print(array)
-        self.assertEqual(array2,expected)
-    def test_concat_array_length_simple(self):
-       array = [3]
-       array2 = [1]
-       array2 = app.concat_arrays(array, array2)
-       expected = [3, 1]
-       print(array)
-       self.assertEqual(array,expected)
-    def test_concat_array_length_complex(self):
-       array = [3, 4, 6]
-       array2 = [1, 2, 3]
-       array2 = app.concat_arrays(array, array2)
-       expected =[3, 4, 6, 1, 2, 3]
-       print(array)
-       self.assertEqual(array,expected)
-    def test_np_uncode_invalidinput(self):
-        x = "blah"
-        result = app.np_uncode(x)
-        expected = None
-        print(result)
-        self.assertEqual(result,expected)
-    # def test_flatten_invalid(self):  # need to make sure it is an float
-    #     x = "blah"
-    #     result = app.np_uncode(x)
-    #     expected = 1
-    #     print(result)
-    #     self.assertEqual(result,expected)
-    # def test_np_uncode_integer(self):  # need to make sure it an integer
-    #     x = 1
-    #     result = app.np_uncode(x)
-    #     expected = 1
-    #     print(result)
-    #     self.assertEqual(result,expected)
-    # def test_np_uncode_float(self):  # need to make sure it is an float
-    #     x = 1.
-    #     result = app.np_uncode(x)
-    #     expected = 1
-    #     print(result)
-    #     self.assertEqual(result,expected)
-    # def test_flatten_simple(self):
-    #     nums = [1, 2, 3, 4, [6, 7, 8], 9, 10]
-    #     my_iterator = iter(nums)
-    #     result = app.flatten(my_iterator)
-    #     expected = [1, 2, 3, 4, 6, 7, 8, 9, 10]
-    #     print(result)
-    #     self.assertEqual(result,expected)
-    # def test_flatten_simple_one(self):
-    #     x = [1]
-    #     result = app.flatten(x)
-    #     expected = 1
-    #     print(result)
-    #     self.assertEqual(result,expected)
-#i love to eat people 
+
     if __name__ == '__main__':
         unittest.main()
