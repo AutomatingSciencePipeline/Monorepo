2a02be4169a724ebaecc10601dcb3bf45ea6b6f4
Robb <computerguy440+gh@gmail.com>
2023-01-27 00:02:05 -0500

    Info about weird error that doesn't seem to affect anything

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
+// TODO VSCode is detecting error "Parsing error: Cannot find module 'next/babel'" but the fix below just causes ESLint errors:
+// https://stackoverflow.com/questions/71662525/failed-to-load-config-next-babel-to-extend-from-eslintrc-json
+// https://nextjs.org/docs/api-reference/next.config.js/introduction
+
 const nextConfig = {
 	reactStrictMode: true,
 };
