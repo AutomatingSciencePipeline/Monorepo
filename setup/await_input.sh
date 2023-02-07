7ea05735ab433b6d28968dec3793ced95dda6889
Robb <computerguy440+gh@gmail.com>
2023-02-07 16:36:06 -0500

    Script to pause and allow resuming execution afterwards

---
+++ setup/await_input.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# https://www.linuxquestions.org/questions/linux-general-1/how-to-make-shell-script-wait-for-key-press-to-proceed-687491/
+read -p "Read the above, then press any key to proceed..." -r -n 1 -s
