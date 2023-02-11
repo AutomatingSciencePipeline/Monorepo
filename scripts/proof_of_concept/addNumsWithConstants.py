4a875951057f51760755929dcc51f3a9ec469d9c
David Sampson <davidcsampson@gmail.com>
2023-02-11 15:17:24 -0500

    Made Dumb text dumber and other things smarter
    had to update the code that retrieves variable names and values to allow it to work across sections

--- scripts/proof_of_concept/addNumsWithConstants.py
+++ scripts/proof_of_concept/addNumsWithConstants.py
@@ -1,33 +1,33 @@
 import csv
 import sys
-import os
 import configparser
 
 from random import shuffle
+
 
 def shuffle_word(word):
     word = list(word)
     shuffle(word)
     return ''.join(word)
 
+
 def main():
     config = configparser.ConfigParser()
     args = sys.argv[1:]
     configFile = args[0]
     config.read(configFile)
-    x = int(config["DEFAULT"]["x"])
-    y = int(config["DEFAULT"]["y"])
-    a = int(config["DEFAULT"]["a"])
-    b = float(config["DEFAULT"]["b"])
-    c = config["DEFAULT"]["c"]
-    invert = config.getboolean("DEFAULT",'invert')
-    add = x + y + a + b
+    x: int = int(config["DEFAULT"]["x"])
+    y: int = int(config["DEFAULT"]["y"])
+    a: int = int(config["const"]["a"])
+    b: float = float(config["const"]["b"])
+    c: str = config['Strings']["c"]
+    invert: bool = config.getboolean("const", 'invert')
+    add: float = x + y + a + b
     sub = x - y + a + b
-    with open('AddNumResult.csv', 'w') as result:
+    with open('AddNumResult.csv', 'w', encoding="utf8") as result:
         writer = csv.writer(result)
-        writer.writerow(['Addition', 'Subtraction','scrambled'])
-        writer.writerow([add*-1 if invert  else add, sub*-1 if invert  else sub, shuffle_word(c)])
-
+        writer.writerow(['Addition', 'Subtraction', 'scrambled'])
+        writer.writerow([add * -1 if invert else add, sub * -1 if invert else sub, shuffle_word(c)])
 
     print("done")
     return 0
