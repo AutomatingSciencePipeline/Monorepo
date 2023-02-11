e0dc3c5769636265bacdda686c0b5636cad25541
Robb <computerguy440+gh@gmail.com>
2023-02-11 18:24:02 -0500

    Organize our example experiments. Make addNumsTimed have a parameterized wait time

--- example_experiments/python/addNumsTimed.py
+++ example_experiments/python/addNumsTimed.py
@@ -2,15 +2,27 @@ import sys
 import configparser
 import time
 
+# pylint: disable-next=pointless-string-statement
+"""
+Example settings for a successful run:
+
+Parameters:
+x, 1, 1, 10, 1
+y, 1, 1, 10, 1
+waitTime 3, 3, 3, 3
+
+"""
+
 def main():
-    print("waiting 10 seconds...")
-    time.sleep(10)
     config = configparser.ConfigParser()
     args = sys.argv[1:]
     configFile = args[0]
     config.read(configFile)
     x = int(config["DEFAULT"]["x"])
     y = int(config["DEFAULT"]["y"])
+    waitTime = int(config["DEFAULT"]["waitTime"])
+    print(f"waiting {waitTime} seconds...")
+    time.sleep(waitTime)
     result = x+y
     print(f"done with result: {result}")
     print(result)
