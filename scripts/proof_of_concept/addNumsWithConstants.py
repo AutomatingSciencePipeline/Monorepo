9f3d57225c45bdc65c9ff50fb3977b476c87e0b2
Robb <computerguy440+gh@gmail.com>
2023-02-11 17:16:17 -0500

    Example inputs for addNumsWithConstants

--- scripts/proof_of_concept/addNumsWithConstants.py
+++ scripts/proof_of_concept/addNumsWithConstants.py
@@ -4,6 +4,24 @@ import configparser
 
 from random import shuffle
 
+# pylint: disable-next=pointless-string-statement
+"""
+Example settings for a successful run:
+
+Parameters:
+x, 1, 1, 10, 1
+y, 1, 1, 10, 1
+
+Paste into the User-Defined Contants field
+
+[const]
+a = -1
+;test comment
+b = 10.5
+invert = False
+[Strings]
+c = Test String
+"""
 
 def shuffle_word(word):
     word = list(word)
