2eac6304848011b335506343ee88eea6351f61ad
robinsr2 <robinsr2@rose-hulman.edu>
2023-03-16 15:51:11 -0400

    Add more gen_config test ideas

--- apps/backend/tests/test_config.py
+++ apps/backend/tests/test_config.py
@@ -2,15 +2,8 @@ import unittest
 
 from modules.configs import gather_parameters
 
-test_empty_hyperparams_dict_list = [] #Empty dictionary
-test_one_hyperparams_dict_list = [{
-      "name": "x",
-      "default": "1",
-      "min": "1",
-      "max": "5",
-      "step": "1",
-      "type": "integer"
-    }]
+test_empty_hyperparams_dict_list = []  #Empty dictionary
+test_one_hyperparams_dict_list = [{"name": "x", "default": "1", "min": "1", "max": "5", "step": "1", "type": "integer"}]
 test_empty_dict_list = {}
 test_empty_list = []
 
@@ -19,14 +12,16 @@ def test_gather_parameters_no_params():
     #Do we want to handle checking for not having any hyperparams in the array
     gather_parameters(test_empty_hyperparams_dict_list, test_empty_dict_list, test_empty_list)
 
+
 def test_gather_parameters_one_param():
     gather_parameters(test_one_hyperparams_dict_list, test_empty_dict_list, test_empty_list)
+
 
 #Tests to write-- (gather_parameters)
 #Missing type (throws exception)
 #Missing name (throws exception)
 #Missing min and max for integer and floats (throws exception)
-#Missing default for string (throws exception) 
+#Missing default for string (throws exception)
 #Else case check parameters to see if hyperparameter is there
 #Happy case for min == max for integer float
 #Happy case for min != max for integer float
@@ -43,6 +38,13 @@ def test_gather_parameters_one_param():
 
 #Tests to write-- (generate_config_files)
 #Error with default (returns None) Should we reraise the exception?
-#Error
+#Error making permutations ("Error while making permutations")
+#Successful config generation
+#Do we want to test the configFile writing?
+
+#Tests to write-- (get_config_paramNames)
+#Test for element being/not being in res
 
 
+if __name__ == '__main__':
+    unittest.main()
