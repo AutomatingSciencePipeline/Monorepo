fa22f05664f740aaa5865a195e6fc36ff8751956
David Sampson <davidcsampson@gmail.com>
2023-03-17 16:36:49 -0400

    Reduced more code duplication, also added in numResults functinality though its kinda janky

--- apps/backend/modules/runner.py
+++ apps/backend/modules/runner.py
@@ -52,29 +52,18 @@ def run_trial(experiment_path, config_path, filetype, trialRun: int, keepLogs: b
             return get_data(process, trialRun, keepLogs, trialTimeout)
 
 
-def get_first_line_of_trial_results_csv(filename):  #Header of a 2 line result csv
-    #TODO collapse with below function
-    try:
-        with open(filename, mode='r', encoding="utf8") as file:
-            reader = csv.reader(file)
-            for line in reader:
-                return line
-    except Exception as err:
-        raise GladosUserError("Failed to read trial results csv first line, does the file exist? Typo in the user-specified output filename(s)?") from err
-
-
-def get_second_line_of_trial_results_csv(filename):  #Values of a 2 line result csv
+def get_line_n_of_trial_results_csv(lineNum, filename):
     try:
         with open(filename, mode='r', encoding="utf8") as file:
             reader = csv.reader(file)
             i = 0
             for line in reader:
-                if i == 1:
+                if i == lineNum:
                     return line
                 i += 1
+            raise GladosUserError(f"{filename} is either empty or only has one line")
     except Exception as err:  #TODO add more exception handling for blank file and 1 line file
         raise GladosUserError("Failed to read trial results csv, does the file exist? Typo in the user-specified output filename(s)?") from err
-
 
 def add_to_output_batch(fileOutput, ExpRun):
     try:
@@ -88,6 +77,7 @@ def conduct_experiment(expId, expRef, trialExtraFile, trialResult, filepath, fil
 
     passes = 0
     fails = 0
+    numOutputs = None
     with open('results.csv', 'w', encoding="utf8") as expResults:
         paramNames = get_config_paramNames('configFiles/0.ini')
         writer = csv.writer(expResults)
@@ -101,17 +91,20 @@ def conduct_experiment(expId, expRef, trialExtraFile, trialResult, filepath, fil
                 print(f'Internal Trial Failure {err}')  # TODO should this halt all further experiment runs?
                 fails += 1
                 expRef.update({'fails': fails})
-                if trialNum == 0:
+                if numOutputs is not None:  #After the first trial this should be defined not sure how else to do this
+                    writer.writerow([trialNum] + ["ERROR" for i in range(numOutputs)] + get_configs_ordered(f'configFiles/{trialNum}.ini', paramNames))
+                if trialNum == 0:  #First Trial Failure Abort
                     message = f"First trial of {expId} ran into an error while running, aborting the whole experiment. Read the traceback to find out what the actual cause of this problem is (it will not necessarily be at the top of the stack trace)."
                     print(message)
                     raise ExperimentAbort(message) from err
                 continue
-            except TrialTimeoutError as timeoutErr:
+            except TrialTimeoutError as timeoutErr:  #First Trial timeout Abort
                 print(f"Trial#{trialNum} timed out")
                 fails += 1
                 expRef.update({'fails': fails})
                 #TODO: uncomment when datatypes are added back in again
-                # writer.writerow([trialNum] + ["TIMEOUT" for i in range(numOutputs)] + get_configs_ordered(f'configFiles/{trialNum}.ini', paramNames))
+                if numOutputs is not None:  #After the first trial this should be defined not sure how else to do this
+                    writer.writerow([trialNum] + ["TIMEOUT" for i in range(numOutputs)] + get_configs_ordered(f'configFiles/{trialNum}.ini', paramNames))
                 if trialNum == 0:
                     message = f"First trial of {expId} timed out into an error while running, aborting the whole experiment."
                     print(message)
@@ -121,18 +114,19 @@ def conduct_experiment(expId, expRef, trialExtraFile, trialResult, filepath, fil
             endSeconds = time.time()
             timeTakenMinutes = (endSeconds - startSeconds) / 60
             #Estimating time for all experiments to run and informing frontend
-            if trialNum == 0:
+
+            if trialNum == 0:  #Setting up the Headers of the CSV
                 estimatedTotalTimeMinutes = timeTakenMinutes * numTrialsToRun
                 print(f"Estimated minutes to run: {estimatedTotalTimeMinutes}")
                 expRef.update({'estimatedTotalTimeMinutes': estimatedTotalTimeMinutes})
                 #Setting up the header for the Result CSV
                 if trialResult == '':
                     writer.writerow(["Experiment Run", "Result"] + paramNames)
-                    # numOutputs = 1
+                    numOutputs = 1
                 else:
-                    if (output := get_first_line_of_trial_results_csv(trialResult)) is None:
+                    if (output := get_line_n_of_trial_results_csv(0,trialResult)) is None:
                         raise InternalTrialFailedError("Nothing returned when trying to get header results (David, improve this error message please)")
-                    # numOutputs = len(output)
+                    numOutputs = len(output)
                     writer.writerow(["Experiment Run"] + output + paramNames)
 
             if trialExtraFile != '':
@@ -141,7 +135,7 @@ def conduct_experiment(expId, expRef, trialExtraFile, trialResult, filepath, fil
             if trialResult == '':
                 writer.writerow([trialNum, response_data] + get_configs_ordered(f'configFiles/{trialNum}.ini', paramNames))
             else:
-                output = get_second_line_of_trial_results_csv(trialResult)
+                output = get_line_n_of_trial_results_csv(1,trialResult)
                 if output is None:
                     raise InternalTrialFailedError("Nothing returned when trying to get first non-header line of results (the rest of the runs?) (David, improve this error message please)")
                 writer.writerow([trialNum] + output + get_configs_ordered(f'configFiles/{trialNum}.ini', paramNames))
