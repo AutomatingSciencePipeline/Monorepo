0b4ce9be6f941fb63808bf34554f19e9fa4adf7b
Robb <computerguy440+gh@gmail.com>
2023-03-29 18:57:44 -0400

    Show archived experiments by default

--- apps/frontend/pages/dashboard.tsx
+++ apps/frontend/pages/dashboard.tsx
@@ -422,7 +422,7 @@ const ExperimentList = ({ experiments, onCopyExperiment }: ExperimentListProps) 
 	};
 
 	const [includeCompleted, setIncludeCompleted] = useState(true);
-	const [includeArchived, setIncludeArchived] = useState(false);
+	const [includeArchived, setIncludeArchived] = useState(true);
 
 	return <div className='bg-white lg:min-w-0 lg:flex-1'>
 		<div className='pl-4 pr-6 pt-4 pb-4 border-b border-t border-gray-200 sm:pl-6 lg:pl-8 xl:pl-6 xl:pt-6 xl:border-t-0'>
