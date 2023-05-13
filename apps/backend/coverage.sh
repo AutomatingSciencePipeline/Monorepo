e91f2f7e293ce21f4ab3a48eaf06265bb5ca2fb0
Robb <computerguy440+gh@gmail.com>
2023-05-12 21:20:51 -0400

    Switch coverage script to the approach Riley and David found; fixes needing to specify each folder. Produce HTML report too. Explain flags.

--- apps/backend/coverage.sh
+++ apps/backend/coverage.sh
@@ -4,20 +4,21 @@
 # Avoid pollution from past runs
 rm -f .coverage
 rm -f coverage.xml
+rm -rf coverageReport/
 
-# TODO Can't find docs on how to automatically detect subdirectories? For now they are manually listed :(
-# https://pytest-cov.readthedocs.io/en/latest/config.html
-
-python -m pytest --cov-report xml:coverage.xml \
+# Running `python -m pytest` and not `pytest` because we need the current dir on sys.path, see https://docs.pytest.org/en/6.2.x/usage.html#calling-pytest-through-python-m-pytest
+# -rA flag controls printed report details: https://docs.pytest.org/en/6.2.x/usage.html#detailed-summary-report
+# --cov flags from pytest-cov plugin control contents and file output of the report https://pytest-cov.readthedocs.io/en/latest/readme.html
+python -m pytest \
+    -rA \
+    --cov-report xml:coverage.xml \
+    --cov-report html:coverageReport \
     --cov-branch \
     --cov . \
-    --cov modules/ \
-    --cov modules/data/ \
-    --cov modules/db/ \
-    --cov modules/logging/ \
-    --cov modules/output/
+    --cov modules/
 
 # Print information in the console for the user as well
+# -m also prints Missed lines
 coverage report -m
 
 echo "▶ Use the VSCode action 'Coverage Gutters: Display Coverage' to see coverage in the editor"
