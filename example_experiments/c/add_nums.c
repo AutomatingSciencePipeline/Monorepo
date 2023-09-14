cf577125239566395ad27a8b10699021afc7c1a2
Zhang <zhangam@rose-hulman.edu>
2023-09-14 19:22:18 -0400

    added ini stuff

---
+++ example_experiments/c/add_nums.c
@@ -0,0 +1,11 @@
+/* Example: Add 2 nums. Requires ini.h and ini.c*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "./include/ini.h"
+
+// INI file parser here: https://github.com/benhoyt/inih
+int main(int argc, char *argv[]) {
+    printf("hello\n");
+}
