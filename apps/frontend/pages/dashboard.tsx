20e610db3d2d7801c275185e5399997c41977a71
rhit-leey2 <leey2@rose-hulman.edu>
2023-10-11 14:14:49 -0400

    Cleaning dashboard

--- apps/frontend/pages/dashboard.tsx
+++ apps/frontend/pages/dashboard.tsx
@@ -470,7 +470,7 @@ const ExperimentList = ({ experiments, onCopyExperiment, onDeleteExperiment }: E
 	const [includeCompleted, setIncludeCompleted] = useState(true);
 	const [includeArchived, setIncludeArchived] = useState(true);
 
-	return <div className='bg-white lg:min-w-0 lg:flex-1'>
+	return (<div className='bg-white lg:min-w-0 lg:flex-1'>
 		<div className='pl-4 pr-6 pt-4 pb-4 border-b border-t border-gray-200 sm:pl-6 lg:pl-8 xl:pl-6 xl:pt-6 xl:border-t-0'>
 			<div className='flex items-center'>
 				<h1 className='flex-1 text-lg font-medium'>Projects</h1>
@@ -599,6 +599,6 @@ const ExperimentList = ({ experiments, onCopyExperiment, onDeleteExperiment }: E
 				);
 			})}
 		</ul>
-	</div>;
+	</div>);
 };
 
