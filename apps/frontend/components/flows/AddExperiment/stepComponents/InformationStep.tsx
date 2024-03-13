65626851957b68883a8a98fd477b4332a2ffd44f
Lyra Lee <leey2@rose-hulman.edu>
2024-03-13 15:04:49 -0400

    Replaced prime react components to creatable component for tag impl

--- apps/frontend/components/flows/AddExperiment/stepComponents/InformationStep.tsx
+++ apps/frontend/components/flows/AddExperiment/stepComponents/InformationStep.tsx
@@ -1,7 +1,18 @@
-import { Fragment } from 'react';
-import { InputSection } from '../../../InputSection';
+import { Fragment } from "react";
+import { InputSection } from "../../../InputSection";
+import CreatableSelect from 'react-select/creatable';
 
 export const InformationStep = ({ form, ...props }) => {
+	//add the tag options for the tags
+	// https://react-select.com/creatable
+	// Create a function that returns the tag options fetches from the mongodb
+	const tagOptions = [
+		{ value: 'java', label: 'java' },
+		{ value: 'python', label: 'python' },
+		{ value: 'c', label: 'c' },
+		// Add more tags if needed
+	];
+
 	return (
 		<div className='h-full flex flex-col space-y-6 py-6 sm:space-y-0 sm:divide-y sm:divide-gray-200 sm:py-0'>
 			<Fragment>
@@ -25,8 +36,16 @@ export const InformationStep = ({ form, ...props }) => {
 					</div>
 				</InputSection>
 
-				<InputSection header={'Trial Result'}>
-					<div className='sm:col-span-4'>
+				<InputSection header={"Tag"}>
+					<div className="sm:col-span-4">
+						<div className="sm:col-span-4">
+							<CreatableSelect {...form.getInputProps("tag")} isMulti options={tagOptions} className="block w-full rounded-md border border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm"/>
+						</div>
+					</div>
+				</InputSection>
+
+				<InputSection header={"Trial Result"}>
+					<div className="sm:col-span-4">
 						<input
 							type='text'
 							placeholder='Name and extension of the experiment results file'
