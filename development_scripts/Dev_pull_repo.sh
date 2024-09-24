c28773581b05c5c630ed82fbb4f5a9202ec91661
Riley Windsor <windsors@rose-hulman.edu>
2024-09-24 18:27:16 -0400

    add new scripts

---
+++ development_scripts/Dev_pull_repo.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# This script will pull the most recent version of the development branch and update a couple of file to prepare them for debugging
+
+git clone https://github.com/AutomatingSciencePipeline/Monorepo.git
+
+echo 'You will need to copy in .env and kubernetes_secrets file!'
+
+git checkout development
+
+git pull origin development
+
+sed -i 's/glados-backend:latest/glados-backend:development/g' ./kubernetes_init/backend/deployment-backend.yaml
+sed -i 's/glados-frontend:latest/glados-frontend:development/g' ./kubernetes_init/frontend/deployment-frontend.yaml
