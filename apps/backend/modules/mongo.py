c3b8e40827ab5e744a195eeefa8b3e60049033d4
Riley Windsor <windsors@rose-hulman.edu>
2024-09-30 23:19:43 -0400

    Update mongo.py

--- apps/backend/modules/mongo.py
+++ apps/backend/modules/mongo.py
@@ -43,26 +43,29 @@ def upload_log_file(experimentId: str, contents: str, mongoClient: pymongo.Mongo
         raise Exception("Encountered error while storing log file in MongoDB") from err
     
 def check_insert_default_experiments(mongoClient: pymongo.MongoClient):
+    # this gets run on its own thread, so let it try to enter the default experiments
+    def insertExperiments():
+        defaultExperimentCollection = mongoClient["gladosdb"].defaultExperiments
+        experiments = [
+            # python experiments
+            {"name": "addNums.py", "type": "python", "url": "https://raw.githubusercontent.com/AutomatingSciencePipeline/Monorepo/refs/heads/main/example_experiments/python/addNums.py"},
+            {"name": "addNumsFailsOnXis1Yis5.py", "type": "python", "url": "https://raw.githubusercontent.com/AutomatingSciencePipeline/Monorepo/refs/heads/main/example_experiments/python/addNumsFailsOnXis1Yis5.py"},
+            {"name": "addNumsTimeOutOnXis1Yis5.py", "type": "python", "url": "https://raw.githubusercontent.com/AutomatingSciencePipeline/Monorepo/refs/heads/main/example_experiments/python/addNumsTimeOutOnXis1Yis5.py"},
+            {"name": "addNumsTimed.py", "type": "python", "url": "https://raw.githubusercontent.com/AutomatingSciencePipeline/Monorepo/refs/heads/main/example_experiments/python/addNumsTimed.py"},
+            {"name": "addNumsWithConstants.py", "type": "python", "url": "https://raw.githubusercontent.com/AutomatingSciencePipeline/Monorepo/refs/heads/main/example_experiments/python/addNumsWithConstants.py"},
+            {"name": "alwaysFail.py", "type": "python", "url": "https://raw.githubusercontent.com/AutomatingSciencePipeline/Monorepo/refs/heads/main/example_experiments/python/alwaysFail.py"},
+            {"name": "genetic_algorithm.py", "type": "python", "url": "https://raw.githubusercontent.com/AutomatingSciencePipeline/Monorepo/refs/heads/main/example_experiments/python/genetic_algorithm.py"}
+            # C experiments
+            # Java experiments
+        ]
+        
+        for exp in experiments:
+            count = defaultExperimentCollection.count_documents({"name": exp["name"]})
+            if count == 0:
+                defaultExperimentCollection.insert_one(exp)
+                
     try:
-        verify_mongo_connection(mongoClient)
+        insertExperiments()
     except:
-        # wait somehow...
-        check_insert_default_experiments(mongoClient)
-    defaultExperimentCollection = mongoClient["gladosdb"].defaultExperiments
-    experiments = [
-        # python experiments
-        {"name": "addNums.py", "type": "python", "url": "https://raw.githubusercontent.com/AutomatingSciencePipeline/Monorepo/refs/heads/main/example_experiments/python/addNums.py"},
-        {"name": "addNumsFailsOnXis1Yis5.py", "type": "python", "url": "https://raw.githubusercontent.com/AutomatingSciencePipeline/Monorepo/refs/heads/main/example_experiments/python/addNumsFailsOnXis1Yis5.py"},
-        {"name": "addNumsTimeOutOnXis1Yis5.py", "type": "python", "url": "https://raw.githubusercontent.com/AutomatingSciencePipeline/Monorepo/refs/heads/main/example_experiments/python/addNumsTimeOutOnXis1Yis5.py"},
-        {"name": "addNumsTimed.py", "type": "python", "url": "https://raw.githubusercontent.com/AutomatingSciencePipeline/Monorepo/refs/heads/main/example_experiments/python/addNumsTimed.py"},
-        {"name": "addNumsWithConstants.py", "type": "python", "url": "https://raw.githubusercontent.com/AutomatingSciencePipeline/Monorepo/refs/heads/main/example_experiments/python/addNumsWithConstants.py"},
-        {"name": "alwaysFail.py", "type": "python", "url": "https://raw.githubusercontent.com/AutomatingSciencePipeline/Monorepo/refs/heads/main/example_experiments/python/alwaysFail.py"},
-        {"name": "genetic_algorithm.py", "type": "python", "url": "https://raw.githubusercontent.com/AutomatingSciencePipeline/Monorepo/refs/heads/main/example_experiments/python/genetic_algorithm.py"}
-        # C experiments
-        # Java experiments
-    ]
-    
-    for exp in experiments:
-        count = defaultExperimentCollection.count_documents({"name": exp["name"]})
-        if count == 0:
-            defaultExperimentCollection.insert_one(exp)
+        # keep trying
+        insertExperiments()
