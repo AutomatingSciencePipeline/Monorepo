8d13f384d194c23332836ffbe9a31f31be45b965
Riley Windsor <windsors@rose-hulman.edu>
2024-10-02 21:48:01 -0400

    stupid env file

--- apps/frontend/components/flows/AddExperiment/stepComponents/DispatchStep.tsx
+++ apps/frontend/components/flows/AddExperiment/stepComponents/DispatchStep.tsx
@@ -21,7 +21,7 @@ const SUPPORTED_FILE_TYPES = {
 export const DispatchStep = ({ id, form, ...props }) => {
 	const { userId } = useAuth();
 	const [loading, setLoading] = useState<boolean>(false);
-	const BACKEND_PORT = env.GLADOS_SERVICE_BACKEND_SERVICE_PORT;
+	const BACKEND_PORT = env.BACKEND_PORT;
 	console.log(BACKEND_PORT);
 
 	const onDropFile = (files: Parameters<DropzoneProps['onDrop']>[0]) => {
