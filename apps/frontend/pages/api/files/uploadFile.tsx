3635a3daf64c3d5ae8a0d49ef75b2e86ab48ba19
rhit-windsors <windsors@rose-hulman.edu>
2024-10-15 15:38:41 -0400

    update frontend

--- apps/frontend/pages/api/files/uploadFile.tsx
+++ apps/frontend/pages/api/files/uploadFile.tsx
@@ -11,11 +11,6 @@ const mongoFileUploader: NextApiHandler<string> = async (req, res) => {
             return res.status(400).json({ response: "Not enough arguments!" } as any);
         }
 
-        if (typeof fileToUpload !== 'string') {
-            res.status(400).json({ response: 'File is not in the correct format' } as any);
-            return;
-        }
-
         try {
             const client = await clientPromise;
             const db = client.db(DB_NAME);
@@ -30,7 +25,7 @@ const mongoFileUploader: NextApiHandler<string> = async (req, res) => {
                     chunkSizeBytes: 1048576,
                     metadata: { field: 'expId', value: experimentId }
                 }) as any);
-                
+
 
             res.status(200).json({ response: 'Successfully wrote file!' } as any);
             return;
