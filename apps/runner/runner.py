6035a5656e4513ee38fea445ce4523b0db598dae
Riley Windsor <windsors@rose-hulman.edu>
2024-10-18 21:02:16 -0400

    Update runner.py

--- apps/runner/runner.py
+++ apps/runner/runner.py
@@ -201,7 +201,7 @@ def download_experiment_files(experiment: ExperimentData):
         # try to call the backend to download
         url = f'http://glados-service-backend:{os.getenv("BACKEND_PORT")}/downloadExpFile?expId={experiment.expId}'
         response = requests.get(url, timeout=60)
-        file_contents = response.json()["contents"]
+        file_contents = str(base64.b64decode(response.json()["contents"]), "utf-8")
         # write the file contents to file path
         with open(filepath, "x") as file:
             file.write(file_contents)
