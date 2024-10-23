7e190b863b6c98c92d42d689400085ed8c9f69bd
bveal <bveal52@gmail.com>
2024-10-22 22:39:21 -0400

    update UI to only show graph button if project is finished

--- apps/frontend/components/flows/ViewExperiment/ExperimentListing.tsx
+++ apps/frontend/components/flows/ViewExperiment/ExperimentListing.tsx
@@ -248,12 +248,13 @@ export const ExperimentListing = ({ projectinit, onCopyExperiment, onDownloadRes
 				>
         		Delete Experiment
 				</button>
-				<button type="button"
+				{project.finished && <button type="button"
 					className='inline-flex items-center justify-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 xl:w-full'
 					onClick={openGraphModal}
 				>
 					See Graph
 				</button>
+				}
 			</div>
 			<div className='sm:hidden'>
 				<ChevronRightIcon
@@ -316,7 +317,7 @@ export const ExperimentListing = ({ projectinit, onCopyExperiment, onDownloadRes
 					null
 				}
 				{
-                showGraphModal && (
+                (showGraphModal && project.finished) && (
                     <Chart onClose={closeGraphModal} />
                 )
             }
