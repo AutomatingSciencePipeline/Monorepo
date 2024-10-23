00fcb9eb02602c74d1350d5ad1e6c90ac49201bd
rhit-windsors <windsors@rose-hulman.edu>
2024-10-23 13:36:20 -0400

    try casting

--- apps/frontend/lib/mongodb.ts
+++ apps/frontend/lib/mongodb.ts
@@ -18,7 +18,6 @@ export const COLLECTION_RESULTS_CSVS = 'results';
 export const COLLECTION_EXPERIMENTS = 'experiments';
 export const COLLECTION_EXPERIMENT_FILES = 'files';
 
-
 let client: MongoClient;
 let clientPromise: Promise<MongoClient> = new Promise((success) => {
 	return true;
