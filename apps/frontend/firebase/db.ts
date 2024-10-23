fbc6ea51b8426fb8de4223610a9e35be9e57443a
bveal <bveal52@gmail.com>
2024-10-22 22:36:39 -0400

    started basic method to get data for populating chart from database - WIP

--- apps/frontend/firebase/db.ts
+++ apps/frontend/firebase/db.ts
@@ -101,6 +101,25 @@ export const downloadExperimentResults = async (expId: ExperimentDocumentId) => 
 	});
 };
 
+export const getExperimentDataForGraph = async (expId: ExperimentDocumentId) => {
+	console.log(`Getting results for ${expId} to use in graph modal...`);
+	await fetch(`/api/download/csv/${expId}`).then((response) => {
+		if (response?.ok) {
+			return response.json();
+		}
+		return Promise.reject(response);
+	}).catch((response: Response) => {
+		console.warn('Error getting experiment results', response.status);
+		response.json().then((json: any) => {
+			console.warn(json?.response ?? json);
+			const message = json?.response;
+			if (message) {
+				alert(`Error getting experiment results: ${message}`);
+			}
+		});
+	});
+}
+
 export const downloadExperimentProjectZip = async (expId: ExperimentDocumentId) => {
 	console.log(`Downloading project zip for ${expId}...`);
 	await fetch(`/api/download/zip/${expId}`).then((response) => {
