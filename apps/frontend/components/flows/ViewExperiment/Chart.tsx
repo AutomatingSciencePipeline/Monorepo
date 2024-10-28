fa482fae6e7c6470de8bc06c6f7e150524e382ed
Brady Veal <61876166+bveal52@users.noreply.github.com>
2024-10-28 15:40:51 -0400

    data populates with call to api, result csv parsed, and colors generated for graph

--- apps/frontend/components/flows/ViewExperiment/Chart.tsx
+++ apps/frontend/components/flows/ViewExperiment/Chart.tsx
@@ -16,116 +16,104 @@ const ChartModal: React.FC<ChartModalProps> = ({ onClose, project }) => {
     const [chartInstance, setChartInstance] = useState<Chart | null>(null);
     const [canvasKey, setCanvasKey] = useState(0);
     const [chartType, setChartType] = useState<keyof ChartTypeRegistry>('line');
-    const [experimentChartData, setExperimentChartData] = useState<string>('');
+    const [experimentChartData, setExperimentChartData] = useState({ _id: '', experimentId: '', resultContent: '' });
+    const [loading, setLoading] = useState(true);
 
     const setLineChart = () => setChartType('line');
     const setBarChart = () => setChartType('bar');
     const setPieChart = () => setChartType('pie');
 
     useEffect(() => {
-        const fetchData = async () => {
-            try{
-                const data = await getExperimentDataForGraph('A7RIXRMF4jSDz58W5zqm');
-                //console.log(data);
-                return data;
-
-            } catch(error) {
-                console.log("Error fetching data: " + error);
-            }
-        };
-
-        const fetchDataAndSetState = async () => {
-            const results = await fetchData();
-            if (results !== undefined) {
-                setExperimentChartData(results);
-            }
-        };
-
-        fetchDataAndSetState();
-
+        fetch(`/api/download/csv/671a908532c499e45424f25c`).then((response) => response.json()).then((record) => {
+            setExperimentChartData(record);
+            setLoading(false);
+        }).catch((response) => {
+            console.warn('Error getting experiment results', response.status);
+            response.json().then((json: any) => {
+                console.warn(json?.response ?? json);
+                const message = json?.response;
+                if (message) {
+                    alert(`Error getting experiment results: ${message}`);
+                }
+            });
+        }
+        );
     }, [project.expId]);
 
-    console.log(experimentChartData)
+    console.log(experimentChartData.resultContent);
 
-    // const parseCSV = (data: string) => {
-    //     const rows = data.trim().split('\n');
-    //     const headers = rows[0].split(',');
-    
-    //     const xIndex = headers.indexOf('x');
-    //     const yIndex = headers.indexOf('y');
-    
-    //     const xList: number[] = [];
-    //     const yList: number[] = [];
-    
-    //     for (let i = 1; i < rows.length; i++) {
-    //         const cols = rows[i].split(',');
-    //         xList.push(Number(cols[xIndex]));
-    //         yList.push(Number(cols[yIndex]));
-    //     }
-    
-    //     return { xList, yList };
-    // };
-    
-    // const { xList, yList } = parseCSV(csvData);
-    
-    // console.log('xList:', xList);
-    // console.log('yList:', yList);
+   
+    const parseCSV = (data) => {
+        const rows = data.trim().split('\n');
+        const headers = rows[0].split(',');
 
+        const xList = [] as any[];
+        const yList = [] as any[];
+
+        for (let i = 1; i < rows.length; i++) {
+            const cols = rows[i].split(',');
+            xList.push(cols[3]); // Assuming x values are in the 4th column
+            yList.push(Number(cols[4])); // Assuming y values are in the 5th column
+        }
+
+        return { xList, yList };
+    };
+
+    const generateColors = (numColors) => {
+        const colors = [] as string[];
+        for (let i = 0; i < numColors; i++) {
+            const color = `rgba(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, 0.2)`;
+            colors.push(color);
+        }
+        return colors;
+    };
 
     useEffect(() => {
-        const ctx = document.getElementById('myChart') as HTMLCanvasElement;
-        if (chartInstance) {
-            chartInstance.destroy();
-        }
-        const newChartInstance = new Chart(ctx, {
-            type: chartType,
-            data: {
-                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-                datasets: [{
-                    label: '# of Votes',
-                    data: [12, 19, 3, 5, 2, 3],
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
-                    ],
-                    borderColor: [
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
-                    ],
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                scales: {
-                    y: {
-                        beginAtZero: true
+        if (!loading && experimentChartData.resultContent) {
+            const { xList, yList } = parseCSV(experimentChartData.resultContent);
+            const colors = generateColors(xList.length);
+            const ctx = document.getElementById('myChart') as HTMLCanvasElement;
+            if (chartInstance) {
+                chartInstance.destroy();
+            }
+            const newChartInstance = new Chart(ctx, {
+                type: chartType,
+                data: {
+                    labels: xList,
+                    datasets: [{
+                        label: 'Experiment Data',
+                        data: yList,
+                        borderColor: colors,
+                        backgroundColor: colors,
+                    }]
+                },
+                options: {
+                    responsive: true,
+                    scales: {
+                        x: {
+                            display: true,
+                            title: {
+                                display: true,
+                                text: 'X Axis'
+                            }
+                        },
+                        y: {
+                            display: true,
+                            title: {
+                                display: true,
+                                text: 'Y Axis'
+                            }
+                        }
                     }
                 }
-            }
-        });
-        setChartInstance(newChartInstance);
+            });
+            setChartInstance(newChartInstance);
+        }
+    }, [loading, experimentChartData, chartType]);
 
-        // Cleanup function to destroy the chart when the component unmounts or before creating a new chart
-        return () => {
-            if (newChartInstance) {
-                newChartInstance.destroy();
-            }
-        };
-    }, [canvasKey, chartType]);
-
-    // Function to force re-render of the canvas
     const regenerateCanvas = () => {
         setCanvasKey(prevKey => prevKey + 1);
     };
-
 
     return (
         <Modal onClose={() => { onClose(); regenerateCanvas(); }}>
@@ -142,9 +130,13 @@ const ChartModal: React.FC<ChartModalProps> = ({ onClose, project }) => {
             </div>
             <div className='p-4'>
                 <h2 className='text-xl font-bold mb-4'>{project.name}&apos;s Chart</h2>
-                <div key={canvasKey}>
-                    <canvas id='myChart'></canvas>
-                </div>
+                {loading ? (
+                    <p>Loading data...</p>
+                ) : (
+                    <div key={canvasKey}>
+                        <canvas id='myChart'></canvas>
+                    </div>
+                )}
             </div>
         </Modal>
     );
