2b674654a77ca72611aab6efb2754bb9c44e79e6
Riley Windsor <windsors@rose-hulman.edu>
2024-11-05 18:06:26 -0500

    Update listen.tsx

--- apps/frontend/pages/api/experiments/listen.tsx
+++ apps/frontend/pages/api/experiments/listen.tsx
@@ -7,70 +7,67 @@ export const dynamic = "force-dynamic";
 export default async function handler(req, res) {
     const { uid } = req.query;
 
-    if (req.method === "GET") {
-        let responseStream = new TransformStream();
-        const writer = responseStream.writable.getWriter();
-        const encoder = new TextEncoder();
+    let responseStream = new TransformStream();
+    const writer = responseStream.writable.getWriter();
+    const encoder = new TextEncoder();
 
-        // Connect to MongoDB
-        const client = await clientPromise;
-        const db = client.db(DB_NAME);
-        const experimentsCollection = db.collection(COLLECTION_EXPERIMENTS);
+    // Connect to MongoDB
+    const client = await clientPromise;
+    const db = client.db(DB_NAME);
+    const experimentsCollection = db.collection(COLLECTION_EXPERIMENTS);
 
-        // Set up a Change Stream for real-time updates
-        const pipeline = [{ $match: { "fullDocument.creator": uid } }];
-        const options = { fullDocument: "updateLookup" };
-        const changeStream = experimentsCollection.watch(pipeline, options);
+    // Set up a Change Stream for real-time updates
+    const pipeline = [{ $match: { "fullDocument.creator": uid } }];
+    const options = { fullDocument: "updateLookup" };
+    const changeStream = experimentsCollection.watch(pipeline, options);
 
-        // Set up real-time streaming of changes to the client using SSE
-        res.setHeader("Access-Control-Allow-Origin", "*");
-        res.setHeader("Cache-Control", "no-cache");
-        res.setHeader("Connection", "keep-alive");
-        res.setHeader("Content-Type", "text/event-stream");
-        res.setHeader("X-Accel-Buffering", "no")
+    // Set up real-time streaming of changes to the client using SSE
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Cache-Control", "no-cache");
+    res.setHeader("Connection", "keep-alive");
+    res.setHeader("Content-Type", "text/event-stream");
+    res.setHeader("X-Accel-Buffering", "no")
 
-        const HEARTBEAT_INTERVAL = 2000; // 2 seconds (adjust this as needed)
-        const intervalId = setInterval(() => {
-            // Send a heartbeat message to keep the connection alive
-            writer.write(encoder.encode(': heartbeat'));
-        }, HEARTBEAT_INTERVAL);
+    const HEARTBEAT_INTERVAL = 2000; // 2 seconds (adjust this as needed)
+    const intervalId = setInterval(() => {
+        // Send a heartbeat message to keep the connection alive
+        writer.write(encoder.encode(': heartbeat'));
+    }, HEARTBEAT_INTERVAL);
 
-        const initDocs = await experimentsCollection
+    const initDocs = await experimentsCollection
+        .find({ 'creator': uid })
+        .toArray();
+    const initArray = convertToExpsArray(initDocs);
+    writer.write(encoder.encode(JSON.stringify(`data: ${initArray}`)));
+    // res.write(`data: ${JSON.stringify(initArray)}\n\n`);
+
+    // Listen to changes in the collection
+    changeStream.on("change", async () => {
+        const updatedDocuments = await experimentsCollection
             .find({ 'creator': uid })
             .toArray();
-        const initArray = convertToExpsArray(initDocs);
-        writer.write(encoder.encode(JSON.stringify(`data: ${initArray}`)));
-        // res.write(`data: ${JSON.stringify(initArray)}\n\n`);
 
-        // Listen to changes in the collection
-        changeStream.on("change", async () => {
-            const updatedDocuments = await experimentsCollection
-                .find({ 'creator': uid })
-                .toArray();
+        const result = convertToExpsArray(updatedDocuments);
+        // Send the updated experiments to the client
+        // res.write(`data: ${JSON.stringify(result)}\n\n`);
+        writer.write(encoder.encode(JSON.stringify(`data: ${updatedDocuments}`)));
+    });
 
-            const result = convertToExpsArray(updatedDocuments);
-            // Send the updated experiments to the client
-            // res.write(`data: ${JSON.stringify(result)}\n\n`);
-            writer.write(encoder.encode(JSON.stringify(`data: ${updatedDocuments}`)));
-        });
+    // Close the change stream and client connection when the request ends
+    req.socket.on("close", () => {
+        changeStream.close();
+        clearInterval(intervalId);
+        res.end()
+    });
 
-        // Close the change stream and client connection when the request ends
-        req.socket.on("close", () => {
-            changeStream.close();
-            clearInterval(intervalId);
-            res.end()
-        });
+    return new Response(responseStream.readable, {
+        headers: {
+            'Content-Type': 'text/event-stream',
+            Connection: 'keep-alive',
+            'Cache-Control': 'no-cache, no-transform',
+        },
+    });
 
-        return new Response(responseStream.readable, {
-            headers: {
-                'Content-Type': 'text/event-stream',
-                Connection: 'keep-alive',
-                'Cache-Control': 'no-cache, no-transform',
-            },
-        });
-    } else {
-        res.status(405).json({ message: "Method Not Allowed" });
-    }
 }
 
 function convertToExpsArray(arr: WithId<Document>[]) {
