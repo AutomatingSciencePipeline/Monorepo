00c6f9c271fd2ec9066511be9771cef3641e94ff
Riley Windsor <windsors@rose-hulman.edu>
2024-11-05 19:35:27 -0500

    Update ExperimentListing.tsx

--- apps/frontend/app/components/flows/ViewExperiment/ExperimentListing.tsx
+++ apps/frontend/app/components/flows/ViewExperiment/ExperimentListing.tsx
@@ -67,7 +67,10 @@ export const ExperimentListing = ({ projectinit, onCopyExperiment, onDownloadRes
 			const eventSource = new EventSource(`/api/experiments/subscribe?expId=${project.expId}`);
 			eventSource.onmessage = (event) => {
 				console.log("received change!");
-				setProject(JSON.parse(event.data) as ExperimentData);
+				if (JSON.parse(event.data) !== ':heartbeat') {
+					setProject(JSON.parse(event.data) as ExperimentData);
+				}
+
 			}
 			// subscribeToExp(project.expId, (data) => {
 			// 	setProject(data as ExperimentData);
@@ -176,7 +179,7 @@ export const ExperimentListing = ({ projectinit, onCopyExperiment, onDownloadRes
 							<span
 								className="hidden sm:inline-block sm:align-middle sm:h-screen"
 								aria-hidden="true">
-							&#8203;
+								&#8203;
 							</span>
 
 							<div
@@ -238,7 +241,7 @@ export const ExperimentListing = ({ projectinit, onCopyExperiment, onDownloadRes
 					className='inline-flex items-center justify-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 xl:w-full'
 					onClick={openDeleteModal}
 				>
-        		Delete Experiment
+					Delete Experiment
 				</button>
 			</div>
 			<div className='sm:hidden'>
