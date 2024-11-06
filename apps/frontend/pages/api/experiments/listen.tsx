1ad6f52e266c8536db2e3fb6db2769fec6258aec
Riley Windsor <windsors@rose-hulman.edu>
2024-11-06 10:51:48 -0500

    make sure connection gets closed

--- apps/frontend/pages/api/experiments/listen.tsx
+++ apps/frontend/pages/api/experiments/listen.tsx
@@ -57,7 +57,7 @@ export default async function handler(req, res) {
     });
 
     // Close the change stream and client connection when the request ends
-    req.socket.on("close", () => {
+    req.on("close", () => {
         changeStream.close();
         clearInterval(intervalId);
         res.end()
