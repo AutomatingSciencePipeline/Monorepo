d4abbf7bb085e353982f8ee438b3e907244689a0
Riley Windsor <windsors@rose-hulman.edu>
2024-11-10 15:46:48 -0500

    listen to all docs for now

--- apps/frontend/pages/api/experiments/listen.tsx
+++ apps/frontend/pages/api/experiments/listen.tsx
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
     const experimentsCollection = db.collection(COLLECTION_EXPERIMENTS);
 
     // Set up a Change Stream for real-time updates
-    const pipeline = [{ $match: { "creator": uid } }];
+    const pipeline = [];
     const options = { fullDocument: "updateLookup" };
     const changeStream = experimentsCollection.watch(pipeline, options);
 
