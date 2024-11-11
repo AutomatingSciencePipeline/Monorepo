ef52cd616b0103c0a3e42554eb4f2e1fcd453f67
Riley Windsor <windsors@rose-hulman.edu>
2024-11-10 21:48:52 -0500

    Update deploy.sh

--- helm_packages/mongodb-helm/deploy.sh
+++ helm_packages/mongodb-helm/deploy.sh
@@ -2,4 +2,4 @@
 
 echo 'Deploying helm package for MongoDB replica set!'
 
-helm install glados-mongodb oci://registry-1.docker.io/bitnamicharts/mongodb -f ./mongodb-helm/values.yaml
+helm install glados-mongodb oci://registry-1.docker.io/bitnamicharts/mongodb -f values.yaml
