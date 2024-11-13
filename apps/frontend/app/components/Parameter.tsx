7ee8c486d47306e41b24e2c615b69e6d8cdef5e5
bveal <bveal52@gmail.com>
2024-11-13 11:55:51 -0500

    update values UI

--- apps/frontend/app/components/Parameter.tsx
+++ apps/frontend/app/components/Parameter.tsx
@@ -15,56 +15,60 @@ const Parameter = ({ form, type, index, confirmedValues, setConfirmedValues, ...
 	const Component = remains[type];
 
 	const handleConfirm = (values) => {
-        const updatedValues = confirmedValues.map((item) =>
-            item.index === index ? { ...item, values: values } : item
-        );
-        if (!updatedValues.some((item) => item.index === index)) {
-            updatedValues.push({ index, values: values });
-        }
-        setConfirmedValues(updatedValues);
-    };
+		const updatedValues = confirmedValues.map((item) =>
+			item.index === index ? { ...item, values: values } : item
+		);
+		if (!updatedValues.some((item) => item.index === index)) {
+			updatedValues.push({ index, values: values });
+		}
+		setConfirmedValues(updatedValues);
+	};
 
 	const currentConfirmedValues = confirmedValues.find((item) => item.index === index)?.values || [];
-	
+
 	return (
 		<Draggable key={index} index={index} draggableId={index.toString()}>
 			{(provided) => {
 				return (
 					<div>
 						<div
-						ref={provided.innerRef}
-						{...provided.draggableProps}
-						// className={'mt-8 justify-start'}
-						className={'flex items-center mb-2'}
-					>
-						<Center {...provided.dragHandleProps}>
-							<GripVertical className='mr-2' />
-						</Center>
-						<span className='text-gray-500 mr-2'>{type}</span>
-						<input
-							type='text'
-							placeholder='name'
-							className='block w-full rounded-l-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm'
-							{...form.getListInputProps('hyperparameters', index, 'name')}
-							required
-						/>
-						<Component form={form} type={type} index={index} onConfirm={handleConfirm} {...rest} />
-						<ActionIcon
-							color='red'
-							onClick={() => form.removeListItem('hyperparameters', index)}
-							className='ml-2'
+							ref={provided.innerRef}
+							{...provided.draggableProps}
+							// className={'mt-8 justify-start'}
+							className={'flex items-center mb-2'}
 						>
-							<Trash/>
-						</ActionIcon>
-					</div>
+							<Center {...provided.dragHandleProps}>
+								<GripVertical className='mr-2' />
+							</Center>
+							<span className='text-gray-500 mr-2'>{type}</span>
+							<input
+								type='text'
+								placeholder='name'
+								className='block w-full rounded-l-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm'
+								{...form.getListInputProps('hyperparameters', index, 'name')}
+								required
+							/>
+							<Component form={form} type={type} index={index} onConfirm={handleConfirm} {...rest} />
+							<ActionIcon
+								color='red'
+								onClick={() => form.removeListItem('hyperparameters', index)}
+								className='ml-2'
+							>
+								<Trash />
+							</ActionIcon>
+						</div>
 						{currentConfirmedValues.length > 0 && (
-                            <div className='mt-4'>
-                                <h3>Values:</h3>
-                                {currentConfirmedValues.map((value, idx) => (
-                                    <p key={idx}>{value}</p>
-                                ))}
-                            </div>
-                        )}
+							<div className='mt-4 p-4 bg-gray-100 rounded-md shadow-sm'>
+								<h3 className='text-lg font-medium text-gray-700 mb-2'>Values:</h3>
+								{currentConfirmedValues.map((value, idx) => (
+									<div key={idx} className='flex items-center mb-2'>
+										<span className='block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm p-2 bg-white'>
+											{value}
+										</span>
+									</div>
+								))}
+							</div>
+						)}
 					</div>
 				);
 			}}
@@ -106,71 +110,71 @@ const StringParam = ({ form, type, index, onConfirm, ...rest }) => {
 	const [opened, setOpened] = useState(false);
 	const [values, setValues] = useState(['']);
 
-    const handleAddValue = () => {
-        setValues([...values, '']);
-    };
+	const handleAddValue = () => {
+		setValues([...values, '']);
+	};
 
-    const handleChange = (e, idx) => {
-        const newValues = [...values];
-        newValues[idx] = e.target.value;
-        setValues(newValues);
-    };
+	const handleChange = (e, idx) => {
+		const newValues = [...values];
+		newValues[idx] = e.target.value;
+		setValues(newValues);
+	};
 
 	const handleDelete = (idx) => {
-        const newValues = values.filter((_, i) => i !== idx);
-        setValues(newValues);
-    };
+		const newValues = values.filter((_, i) => i !== idx);
+		setValues(newValues);
+	};
 
-    const handleClose = () => {
-		
-        form.setFieldValue(`hyperparameters.${index}.default`, values);
-        setOpened(false);
-    };
+	const handleClose = () => {
+
+		form.setFieldValue(`hyperparameters.${index}.default`, values);
+		setOpened(false);
+	};
 
 	const handleConfirm = () => {
-        onConfirm(values);
+		onConfirm(values);
 		setOpened(false);
-    };
-    return (
-        <>
-			 {/* <div className='mt-4'>
+	};
+	return (
+		<>
+			{/* <div className='mt-4'>
                 <h3>Confirmed Values:</h3>
                 {confirmedValues.map((value, idx) => (
                     <p key={idx}>{value}</p>
                 ))}
             </div> */}
-            <Button onClick={() => setOpened(true)} className='ml-2 rounded-md w-1/6 border border-transparent bg-blue-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2'>
-                Edit Strings
-            </Button>
-            <Modal
-                opened={opened}
-                onClose={handleClose}
-                title="Edit String Values"
-            >
-                {values.map((value, idx) => (
-                    <div key={idx} className='flex items-center mb-2'>
-                        <input
-                            type='text'
-                            placeholder={`Value ${idx + 1}`}
-                            className='block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm'
-                            value={value}
-                            onChange={(e) => handleChange(e, idx)}
-                            required
-                        />
-                        <ActionIcon onClick={() => handleDelete(idx)} color='red' className='ml-2'>
-                            <Trash />
-                        </ActionIcon>
-                    </div>
-                ))}
-                <ActionIcon onClick={handleAddValue} color='blue'>
-                    <Plus />
-                </ActionIcon>
+			<Button onClick={() => setOpened(true)} className='ml-2 rounded-md w-1/6 border border-transparent bg-blue-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2'>
+				Edit Strings
+			</Button>
+			<Modal
+				opened={opened}
+				onClose={handleClose}
+				title="Edit String Values"
+			>
+				{values.map((value, idx) => (
+					<div key={idx} className='flex items-center mb-2'>
+						<input
+							type='text'
+							placeholder={`Value ${idx + 1}`}
+							className='block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm'
+							value={value}
+							onChange={(e) => handleChange(e, idx)}
+							required
+						/>
+						<ActionIcon onClick={() => handleDelete(idx)} color='red' className='ml-2'>
+							<Trash />
+						</ActionIcon>
+					</div>
+				))}
+				<ActionIcon onClick={handleAddValue} color='blue'>
+					<Plus />
+				</ActionIcon>
 				<button onClick={handleConfirm} className='mt-4 px-4 py-2 bg-blue-500 text-white rounded-md'>
-                Confirm
-            </button>
-            </Modal>
-        </>
-    );
+					Confirm
+				</button>
+			</Modal>
+		</>
+	);
 };
 
 export default Parameter;
