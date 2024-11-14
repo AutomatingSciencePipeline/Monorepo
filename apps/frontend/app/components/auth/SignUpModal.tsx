b60efabc4f98e74be3ad0db9f0cbd0ffbacabf4b
Riley Windsor <95257011+rhit-windsors@users.noreply.github.com>
2024-11-14 16:06:23 -0500

    Remove Twitter Signin

--- apps/frontend/app/components/auth/SignUpModal.tsx
+++ apps/frontend/app/components/auth/SignUpModal.tsx
@@ -55,18 +55,6 @@ export const SignUpModal = ({ afterSignUp }) => {
 								</a>
 							</div>
 
-							{/* <div>
-								<a
-									href='#'
-									className='w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50'
-								>
-									<span className='sr-only'>
-										Sign in with Twitter
-									</span>
-									<AiOutlineTwitter className='w-5 h-5' />
-								</a>
-							</div> */}
-
 							<div>
 								<a
 									href='#'
