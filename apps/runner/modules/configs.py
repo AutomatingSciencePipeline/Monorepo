b3ee34149f93545cb75a619f51ac3668bcde3416
Brady Veal <61876166+bveal52@users.noreply.github.com>
2024-11-15 11:53:56 -0500

    update config parsing for string list

--- apps/runner/modules/configs.py
+++ apps/runner/modules/configs.py
@@ -38,43 +38,129 @@ def generate_list(param: Parameter, paramName):
         return []
 
 
+# def generate_config_files(experiment: ExperimentData):
+#     constants = {}
+#     parameters = {}
+#     gather_parameters(experiment.hyperparameters, constants, parameters)
+#     explogger.info("param list: " + str(parameters))
+#     explogger.info("const list: " + str(constants))
+    
+
+#     configDict = {}
+#     configIdNumber = 0
+#     for varyingKey, varyingVar in parameters.items():
+#         explogger.info(f'Keeping {varyingVar} constant')
+#         possibleParamVals = []
+
+#         #Required to do the cross product, since each config is made by
+#         #doing a cross product of lists of name value pairs the default variable needs to be
+#         #a single item list so that there is only one possible value for the default variable
+#         possibleParamVals.append(generate_list(varyingVar, varyingKey))
+
+#         for otherKey, otherVar in parameters.items():
+#             if otherKey != varyingKey:
+#                 possibleParamVals.append([(otherKey, get_default(otherVar))])
+#         try:
+#             permutations = list(itertools.product(*possibleParamVals))
+#         except Exception as err:
+#             raise GladosInternalError("Error while making permutations") from err
+
+#         for thisPermutation in permutations:
+#             configItems = {}
+#             for item in thisPermutation:
+#                 name = item[0]
+#                 value = item[1]
+#                 configItems[name] = value
+#             configItems.update(constants)
+#             configDict[f'config{configIdNumber}'] = ConfigData(data=configItems)
+#             explogger.info(f'Generated config {configIdNumber}')
+#             configIdNumber += 1
+
+#     explogger.info("Finished generating configs")
+#     experiment.configs = configDict
+#     return configIdNumber
+
 def generate_config_files(experiment: ExperimentData):
     constants = {}
     parameters = {}
     gather_parameters(experiment.hyperparameters, constants, parameters)
     explogger.info("param list: " + str(parameters))
     explogger.info("const list: " + str(constants))
-    
 
     configDict = {}
     configIdNumber = 0
-    for varyingKey, varyingVar in parameters.items():
-        explogger.info(f'Keeping {varyingVar} constant')
-        possibleParamVals = []
 
-        #Required to do the cross product, since each config is made by
-        #doing a cross product of lists of name value pairs the default variable needs to be
-        #a single item list so that there is only one possible value for the default variable
-        possibleParamVals.append(generate_list(varyingVar, varyingKey))
+    # Check if there is a string list parameter
+    string_list_key = None
+    string_list_values = []
+    for key, var in parameters.items():
+        if isinstance(var, StringListParameter):
+            string_list_key = key
+            string_list_values = var.values
+            break
 
-        for otherKey, otherVar in parameters.items():
-            if otherKey != varyingKey:
-                possibleParamVals.append([(otherKey, get_default(otherVar))])
-        try:
-            permutations = list(itertools.product(*possibleParamVals))
-        except Exception as err:
-            raise GladosInternalError("Error while making permutations") from err
+    if string_list_key:
+        # Generate configurations for each value in the string list
+        for string_value in string_list_values:
+            for varyingKey, varyingVar in parameters.items():
+                if varyingKey == string_list_key:
+                    continue  # Skip the string list parameter
 
-        for thisPermutation in permutations:
-            configItems = {}
-            for item in thisPermutation:
-                name = item[0]
-                value = item[1]
-                configItems[name] = value
-            configItems.update(constants)
-            configDict[f'config{configIdNumber}'] = ConfigData(data=configItems)
-            explogger.info(f'Generated config {configIdNumber}')
-            configIdNumber += 1
+                explogger.info(f'Keeping {varyingVar} constant with {string_value}')
+                possibleParamVals = []
+
+                # Generate list for the varying parameter
+                possibleParamVals.append(generate_list(varyingVar, varyingKey))
+
+                # Add the string list parameter with the current string value
+                possibleParamVals.append([(string_list_key, string_value)])
+
+                for otherKey, otherVar in parameters.items():
+                    if otherKey != varyingKey and otherKey != string_list_key:
+                        possibleParamVals.append([(otherKey, get_default(otherVar))])
+
+                try:
+                    permutations = list(itertools.product(*possibleParamVals))
+                except Exception as err:
+                    raise GladosInternalError("Error while making permutations") from err
+
+                for thisPermutation in permutations:
+                    configItems = {}
+                    for item in thisPermutation:
+                        name = item[0]
+                        value = item[1]
+                        configItems[name] = value
+                    configItems.update(constants)
+                    configDict[f'config{configIdNumber}'] = ConfigData(data=configItems)
+                    explogger.info(f'Generated config {configIdNumber}')
+                    configIdNumber += 1
+    else:
+        # No string list parameter, generate configurations normally
+        for varyingKey, varyingVar in parameters.items():
+            explogger.info(f'Keeping {varyingVar} constant')
+            possibleParamVals = []
+
+            possibleParamVals.append(generate_list(varyingVar, varyingKey))
+
+            for otherKey, otherVar in parameters.items():
+                if otherKey != varyingKey:
+                    possibleParamVals.append([(otherKey, get_default(otherVar))])
+
+            try:
+                permutations = list(itertools.product(*possibleParamVals))
+            except Exception as err:
+                raise GladosInternalError("Error while making permutations") from err
+
+            for thisPermutation in permutations:
+                configItems = {}
+                for item in thisPermutation:
+                    name = item[0]
+                    value = item[1]
+                    configItems[name] = value
+                configItems.update(constants)
+                configDict[f'config{configIdNumber}'] = ConfigData(data=configItems)
+                explogger.info(f'Generated config {configIdNumber}')
+                configIdNumber += 1
 
     explogger.info("Finished generating configs")
     experiment.configs = configDict
