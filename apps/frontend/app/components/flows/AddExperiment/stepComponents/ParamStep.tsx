6cedd03553123098a8b8edc93d9c29d3018f2bd6
Brady Veal <61876166+bveal52@users.noreply.github.com>
2024-11-16 11:18:54 -0500

    undo setvalue for add - testing

--- apps/frontend/app/components/flows/AddExperiment/stepComponents/ParamStep.tsx
+++ apps/frontend/app/components/flows/AddExperiment/stepComponents/ParamStep.tsx
@@ -10,50 +10,33 @@ export const ParamStep = ({ form, confirmedValues, setConfirmedValues, ...props 
 
 	const [isDropDisabled, setIsDropDisabled] = useState(false);
 
-	// const onDragStart = () => {
-    //     // Reset any state related to dragging if needed
-    //     setIsDropDisabled(false);
-    // };
+	const onDragStart = () => {
+        // Reset any state related to dragging if needed
+        setIsDropDisabled(false);
+    };
 
-    // const onDragEnd = ({ destination, source }) => {
-	// 	if (!destination) return;
+    const onDragEnd = ({ destination, source }) => {
+		if (!destination) return;
 
-    //     const hyperparameters = form.values.hyperparameters;
-    //     const [movedItem] = hyperparameters.splice(source.index, 1);
-    //     hyperparameters.splice(destination.index, 0, movedItem);
+        const hyperparameters = form.values.hyperparameters;
+        const [movedItem] = hyperparameters.splice(source.index, 1);
+        hyperparameters.splice(destination.index, 0, movedItem);
 
-    //     form.setFieldValue('hyperparameters', hyperparameters);
+        form.setFieldValue('hyperparameters', hyperparameters);
 
-    //     // Update confirmedValues indices
-    //     const updatedConfirmedValues = confirmedValues.map(item => {
-    //         if (item.index === source.index) {
-    //             return { ...item, index: destination.index };
-    //         } else if (item.index > source.index && item.index <= destination.index) {
-    //             return { ...item, index: item.index - 1 };
-    //         } else if (item.index < source.index && item.index >= destination.index) {
-    //             return { ...item, index: item.index + 1 };
-    //         }
-    //         return item;
-    //     });
+        // Update confirmedValues indices
+        const updatedConfirmedValues = confirmedValues.map(item => {
+            if (item.index === source.index) {
+                return { ...item, index: destination.index };
+            } else if (item.index > source.index && item.index <= destination.index) {
+                return { ...item, index: item.index - 1 };
+            } else if (item.index < source.index && item.index >= destination.index) {
+                return { ...item, index: item.index + 1 };
+            }
+            return item;
+        });
 
-    //     setConfirmedValues(updatedConfirmedValues);
-    // };
-
-	const addNewItem = (type) => {
-        const newItem = {
-            name: '',
-            default: '',
-            ...((type === 'stringlist') && { values: [''] }),
-            ...((type === 'integer' || type === 'float') && {
-                min: '',
-                max: '',
-                step: '',
-            }),
-            type: type,
-        };
-
-        const updatedHyperparameters = [...form.values.hyperparameters, newItem];
-        form.setFieldValue('hyperparameters', updatedHyperparameters);
+        setConfirmedValues(updatedConfirmedValues);
     };
 
 	return (
@@ -70,10 +53,20 @@ export const ParamStep = ({ form, confirmedValues, setConfirmedValues, ...props 
 									type='button'
 									key={`addNew_${type}`}
 									className='-ml-px relative items-center flex-1 px-6 py-2 last:rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:border-blue-500'
-									onClick={() => {
-										addNewItem(type);
-									}
-								}
+									onClick={() =>{
+										console.log("add new item" + type + " to hyperparameters")
+										form.addListItem('hyperparameters', {
+											name: '',
+											default: '',
+											...((type === 'stringlist') && { values: [''] }),
+											...((type === 'integer' || type === 'float') && {
+												min: '',
+												max: '',
+												step: '',
+											}),
+											type: type,
+										})
+									}}
 								>
 									{type}
 								</button>
@@ -83,7 +76,7 @@ export const ParamStep = ({ form, confirmedValues, setConfirmedValues, ...props 
 				</InputSection>
 
 				<div className={'flex-0 p-4 h-full grow-0'}>
-					<DragDropContext>
+					<DragDropContext onDragStart={onDragStart} onDragEnd={onDragEnd}>
 						<div
 							className='h-full grow-0 max-h-fit mb-4 overflow-y-scroll p-4 border-2 border-gray-300 border-dashed rounded-lg hover:border-gray-400'
 							style={{ maxHeight: '60vh' }}
