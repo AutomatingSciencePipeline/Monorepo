660b4b09eea04877696c89615b4cafd66731e045
Brady Veal <61876166+bveal52@users.noreply.github.com>
2024-11-16 16:20:34 -0500

    dispatch button disappearing on final step

--- apps/frontend/app/components/flows/AddExperiment/NewExperiment.tsx
+++ apps/frontend/app/components/flows/AddExperiment/NewExperiment.tsx
@@ -233,7 +233,7 @@ const NewExperiment = ({ formState, setFormState, copyID, setCopyId, ...rest }) 
 											>
 												{status === FormStates.Info ? 'Cancel' : 'Back'}
 											</button>
-											<button
+											{!(status === FormStates.Dispatch) && <button
 												className='rounded-md w-1/6 border border-transparent bg-blue-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2'
 												{...(status === FormStates.Dispatch ?
 													{
@@ -249,7 +249,7 @@ const NewExperiment = ({ formState, setFormState, copyID, setCopyId, ...rest }) 
 													})}
 											>
 												{status === FormStates.Dispatch ? 'Dispatch' : 'Next'}
-											</button>
+											</button>}
 										</div>
 									</div>
 								</form>
