3df132b7ee9d621338dd1f3f1399f9c980c89fec
Riley Windsor <95257011+rhit-windsors@users.noreply.github.com>
2024-11-18 20:30:32 -0500

    Update next.config.mjs

--- apps/frontend/next.config.mjs
+++ apps/frontend/next.config.mjs
@@ -17,7 +17,7 @@ const nextConfig = {
 			},
 			{
 				protocol: 'https',
-				hostname: 'lh3.googleusercontent.com/',
+				hostname: 'lh3.googleusercontent.com',
 				pathname: '/a/**'
 			}
 		],
