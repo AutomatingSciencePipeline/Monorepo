9f93421f89c52002089096af57ac0dcfd0dfe7ca
Riley Windsor <windsors@rose-hulman.edu>
2024-11-18 20:19:47 -0500

    allow google user photos

--- apps/frontend/next.config.mjs
+++ apps/frontend/next.config.mjs
@@ -15,6 +15,11 @@ const nextConfig = {
 				hostname: 'avatars.githubusercontent.com',
 				pathname: '/u/**'
 			},
+			{
+				protocol: 'https',
+				hostname: 'lh3.googleusercontent.com/',
+				pathname: '/a/**'
+			}
 		],
 	}
 };
