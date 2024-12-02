14dd2b744060dd76dada03a5bc11a32c22821d8f
Riley Windsor <windsors@rose-hulman.edu>
2024-12-02 14:03:58 -0500

    Update DispatchStep.tsx

--- apps/frontend/app/components/flows/AddExperiment/stepComponents/DispatchStep.tsx
+++ apps/frontend/app/components/flows/AddExperiment/stepComponents/DispatchStep.tsx
@@ -120,7 +120,7 @@ export const DispatchStep = ({ id, form, updateId, ...props }) => {
 								<td className="border border-gray-300 px-4 py-2">
 									<button
 										className={`rounded-md w-1/2 border border-transparent py-2 px-4 text-sm font-medium text-white shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-50 focus:ring-offset-2 ${
-											selectedFileId === file._id.toString() ? 'bg-green-500' : 'bg-blue-500 hover:bg-blue-700'
+											selectedFileId === file._id.toString() ? 'bg-green-600' : 'bg-blue-500 hover:bg-blue-700'
 										}`}
 										id={file._id.toString()}
 										onClick={() => {
