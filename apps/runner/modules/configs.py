7376cacf4b0a35b8e3ebcd7e74bd65c4f7d26484
Brady Veal <61876166+bveal52@users.noreply.github.com>
2024-12-09 16:13:24 -0500

    fix enum calls

--- apps/runner/modules/configs.py
+++ apps/runner/modules/configs.py
@@ -43,7 +43,7 @@ def float_range(start, stop, step, decimals):
     while round(current, decimals) < round(stop, decimals):  # Use rounded values for comparison
         yield round(current, decimals)  # Ensure consistent decimal places
         current += step
-        
+
 def get_decimal_places(number):
     """Returns the number of decimal places in a float."""
     if isinstance(number, int):
@@ -52,76 +52,45 @@ def get_decimal_places(number):
 
 def expand_values(param):
     """Expands possible values for a parameter with appropriate decimal precision."""
-    if param["type"] == "integer":
+    if param["type"] == ParamType.INTEGER:
         return list(range(param["min"], param["max"] + 1, param["step"]))
-    elif param["type"] == "float":
+    elif param["type"] == ParamType.FLOAT:
         decimals = max(
             get_decimal_places(param["min"]),
             get_decimal_places(param["max"]),
             get_decimal_places(param["step"]),
         )
         return list(float_range(param["min"], param["max"] + param["step"], param["step"], decimals))
-    elif param["type"] == "stringlist":
+    elif param["type"] == ParamType.STRING_LIST:
         return param.get("values", [])
-    elif param["type"] == "string":
-        return [param["default"]] 
-    elif param["type"] == "bool":
+    elif param["type"] == ParamType.STRING:
+        return [param["default"]]
+    elif param["type"] == ParamType.BOOL:
         return [True, False]
     else:
         return []
+
 def generate_permutations(parameters):
-    """Generates permutations dynamically based on parameter definitions, 
-       using itertools.product and filtering based on default values."""
-    
-    # Prepare all possible values for parameters, including default and expanded ranges
+    """Generates permutations dynamically based on parameter definitions."""
     all_values = []
-    base_vals = {}
-    default_vals = {}
-    
-    
-    for param in parameters:
-      if param["type"] == 'integer' or param["type"] == 'float':
-        base_vals[param["name"]] = param["min"]
-      elif param["type"] == 'stringlist':
-        base_vals[param['name']] = param['values'][0]
-      elif param["type"] == 'bool':
-        base_vals[param["name"]] = param["default"]
-
-          
-    for param in parameters:
-        if param["default"] != -1 or param["default"] != "":
-            default_vals[param["name"]] = [param["default"]]
-        else:
-            default_vals[param["name"]] = expand_values(param)
-          
-    print(base_vals)
-    print(default_vals)
-
     for param in parameters:
         all_values.append(expand_values(param))
     
-    # Generate all permutations using itertools.product
     all_permutations = list(itertools.product(*all_values))
     
-    # Now filter permutations based on the constraints: Default values should remain fixed.
     filtered_permutations = []
     for perm in all_permutations:
         perm_dict = {parameters[i]["name"]: perm[i] for i in range(len(parameters))}
         
-        num_defaults_changed = 0
-        for param in parameters:
-            if param["default"] != -1 or param["default"] != "":
-                if perm_dict[param["name"]] != param["default"]:
-                    num_defaults_changed += 1
-        # else:
-        #     # If all constraints are satisfied, add the permutation
-        #     filtered_permutations.append(perm_dict)
-        
+        # Allow only one deviation from default values
+        num_defaults_changed = sum(
+            1 for param in parameters
+            if param["default"] != "" and perm_dict[param["name"]] != param["default"]
+        )
         if num_defaults_changed <= 1:
             filtered_permutations.append(perm_dict)
 
     print("len filtered: ", len(filtered_permutations))
-            
     return filtered_permutations
 
 # def generate_config_files(experiment: ExperimentData):
@@ -227,7 +196,7 @@ def generate_config_files(experiment: ExperimentData):
         param_list.append(param_dict)
 
     print("param_list: ", param_list)
-    
+
     permutations = generate_permutations(param_list)
 
     for permutation in permutations:
