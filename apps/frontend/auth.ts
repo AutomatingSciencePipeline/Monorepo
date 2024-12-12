4c1abb9723cf8096ba3d5f3202d1ef6ef3c343e4
Riley Windsor <windsors@rose-hulman.edu>
2024-12-11 19:40:11 -0500

    disable keycloak

--- apps/frontend/auth.ts
+++ apps/frontend/auth.ts
@@ -2,12 +2,12 @@ import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
-import Keycloak from "next-auth/providers/keycloak";
+// import Keycloak from "next-auth/providers/keycloak";
 import clientPromise from "./lib/mongodb"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: MongoDBAdapter(clientPromise, {databaseName: 'gladosdb'}),
-  providers: [GitHub, Google, Keycloak], //Add Keycloak here to do local testing
+  providers: [GitHub, Google], //Add Keycloak here to do local testing
   callbacks: {
     authorized: async ({ auth }) => {
       // Logged in users are authenticated, otherwise redirect to login page
