cd2b12c39435725a5705bb0be8b744766f3edd46
Riley Windsor <windsors@rose-hulman.edu>
2024-12-16 14:39:42 -0500

    Update ExperimentListing.tsx

--- apps/frontend/app/components/flows/ViewExperiment/ExperimentListing.tsx
+++ apps/frontend/app/components/flows/ViewExperiment/ExperimentListing.tsx
@@ -251,7 +251,7 @@ export const ExperimentListing = ({ projectData: projectData, onCopyExperiment, 
 						<button type="button"
 							className='inline-flex items-center justify-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 xl:w-full'
 							onClick={() => {
-								onDeleteExperiment(project.expId);
+								openDeleteModal();
 							}}>
 							Delete Experiment
 						</button> :
