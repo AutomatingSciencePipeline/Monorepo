4443225ded1e5bdd4baaecc4c98e032fadd5c237
Bennett Toftner <toftnebj@rose-hulman.edu>
2024-12-15 19:43:14 -0500

    exporting image works sometimes

--- apps/frontend/components/flows/ViewExperiment/Chart.tsx
+++ apps/frontend/components/flows/ViewExperiment/Chart.tsx
@@ -24,6 +24,7 @@ const ChartModal: React.FC<ChartModalProps> = ({ onClose, project }) => {
     const [xAxis, setXAxis] = useState('X');
     //const [yAxis, setYAxis] = useState('Y');
     const [headers, setHeaders] = useState<string[]>([]);
+    const [img, setImg] = useState<Base64URLString>('');
 
     const setLineChart = () => setChartType('line');
     const setBarChart = () => setChartType('bar');
@@ -55,6 +56,12 @@ const ChartModal: React.FC<ChartModalProps> = ({ onClose, project }) => {
 
     //console.log(experimentChartData.resultContent);
 
+    const downloadImage = () => {
+        const a = document.createElement('a');
+        a.href = img;
+        a.download = 'downloaded-chart-image.png';
+        a.click();
+    };
    
     const parseCSV = (data) => {
         const rows = data.trim().split('\n');
@@ -164,7 +171,8 @@ const ChartModal: React.FC<ChartModalProps> = ({ onClose, project }) => {
 
             setHeaders(headers);
 
-            const img = newChartInstance.toBase64Image();
+            setImg(newChartInstance.toBase64Image());
+            
 
         }
     }, [loading, experimentChartData, chartType, xAxis]);
@@ -203,20 +211,25 @@ const ChartModal: React.FC<ChartModalProps> = ({ onClose, project }) => {
                     </div>
                 )}
             </div>
-            <fieldset>
-                {headers.map((header) => (
-                    <div key={header}>
-                        <input
-                            type="radio"
-                            id={header}
-                            onChange={() => setXAxis(header)}
-                            name = "xaxis"
-                            value={header}
-                        />
-                        <label htmlFor={header}>{header}</label>
-                    </div>
-                ))}
-            </fieldset>
+            <div>
+                <fieldset>
+                    {headers.map((header) => (
+                        <div key={header}>
+                            <input
+                                type="radio"
+                                id={header}
+                                onChange={() => setXAxis(header)}
+                                name = "xaxis"
+                                value={header}
+                            />
+                            <label htmlFor={header}>{header}</label>
+                        </div>
+                    ))}
+                </fieldset>
+                <button onClick={downloadImage} className='inline-flex items-center justify-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 xl:w-full'>
+                    Download Image
+                </button>
+            </div>
         </Modal>
     );
 };
