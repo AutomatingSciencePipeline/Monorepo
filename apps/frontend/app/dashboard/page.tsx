86ba6899ec8f2dc2497741cef64f456434c88615
Brady Veal <61876166+bveal52@users.noreply.github.com>
2024-12-17 00:38:51 -0500

    temp remove genetic algo

--- apps/frontend/app/dashboard/page.tsx
+++ apps/frontend/app/dashboard/page.tsx
@@ -494,13 +494,13 @@ export default function DashboardPage() {
 																				>
 																					Multistring Testing (Python)
 																				</button>
-																				<button
+																				{/* <button
 																					type="button"
 																					className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
 																					onClick={() => selectExperiment(ExperimentTypes.GeneticAlgorithm)}
 																				>
 																					Genetic Algorithm
-																				</button>
+																				</button> */}
 																			</div>
 																		</Dialog.Panel>
 																	</Transition.Child>
