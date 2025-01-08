0a8bce52cc30e03a9da9bc80724f0e3ae5703a21
Bennett Toftner <toftnebj@rose-hulman.edu>
2025-01-08 03:39:36 -0500

    checkbox

--- apps/frontend/app/components/flows/ViewExperiment/Chart.tsx
+++ apps/frontend/app/components/flows/ViewExperiment/Chart.tsx
@@ -24,6 +24,7 @@ const ChartModal: React.FC<ChartModalProps> = ({ onClose, project }) => {
     const [headers, setHeaders] = useState<string[]>([]);
     const [img, setImg] = useState<string>('');
     const [isFullscreen, setIsFullscreen] = useState(false);
+    const [aggregateData, setAggregateData] = useState(false);
 
     const toggleFullscreen = () => {
         setIsFullscreen(!isFullscreen);
@@ -222,6 +223,9 @@ const ChartModal: React.FC<ChartModalProps> = ({ onClose, project }) => {
                     ))}
                 </select>
 
+            </div>
+            <div className='p-4'>
+                <label><input id='aggregate-data-box' type="checkbox" checked={aggregateData} onChange={() => setAggregateData(!aggregateData)}></input>Aggregate data?</label>
             </div>
             <button onClick={downloadImage} className='inline-flex items-center justify-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 xl:w-full'>
                 Download Image
