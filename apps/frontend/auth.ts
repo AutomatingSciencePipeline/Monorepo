7e1a6bdbb56b52afebf65263bb4e4bdf8f4229f2
Riley Windsor <windsors@rose-hulman.edu>
2025-01-26 23:06:51 -0500

    Update auth.ts

--- apps/frontend/auth.ts
+++ apps/frontend/auth.ts
@@ -2,7 +2,6 @@ import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
-// import Keycloak from "next-auth/providers/keycloak";
 import clientPromise from "./lib/mongodb"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
