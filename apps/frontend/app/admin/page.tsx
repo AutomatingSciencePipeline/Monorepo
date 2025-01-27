dec07714f803994994e9bb6d3e2617f258b6055d
Riley Windsor <windsors@rose-hulman.edu>
2025-01-26 23:10:49 -0500

    update admin

--- apps/frontend/app/admin/page.tsx
+++ apps/frontend/app/admin/page.tsx
@@ -317,9 +317,6 @@ export default function Page() {
                         <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                             <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                                 <tr>
-                                    <th scope="col" className="px-6 py-3 hidden">
-                                        Id
-                                    </th>
                                     <th scope="col" className="px-6 py-3">
                                         Email
                                     </th>
