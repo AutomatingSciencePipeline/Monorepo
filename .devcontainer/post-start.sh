1a08f5e068290563d1b5ea0fc892eb8d8335aca0
Riley Windsor <95257011+rhit-windsors@users.noreply.github.com>
2025-02-07 21:24:14 +0000

    work on codespaces

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-# Silence warnings about the git repos not being owned by this user (since it's bind mounted from host)
-# This runs inside the container, so it's okay to do global git config
-git config --global --add safe.directory "/workspaces/Monorepo"
-git config --global --add safe.directory "/workspaces/Monorepo/Monorepo.wiki"
-echo Added workspace folder to git safe directories
+cd ./apps/frontend || echo 'folder not found!'
+npm i
+
+minikube config set cpus 3
+minikube config set memory 12000
+
+ctlptl create cluster minikube --registry=ctlptl-registry
