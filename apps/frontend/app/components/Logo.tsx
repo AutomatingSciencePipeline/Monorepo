6609e31065a57dcfd8beb1072a38dccd6462e9c2
rhit-windsors <windsors@rose-hulman.edu>
2025-03-17 13:20:04 -0400

    Update Logo.tsx

--- apps/frontend/app/components/Logo.tsx
+++ apps/frontend/app/components/Logo.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 
 export const Logo = () => {
-	return <Image className='rounded-full' src="/images/glados-logo.png" alt="GLADOS" width={40} height={40} />;
+	return <Image className='rounded-full' src="/images/glados-logo.jpg" alt="GLADOS" width={40} height={40} />;
 	// return <FontAwesomeIcon icon={faFire} className={'text-orange-400 h-8'} />;
 };
