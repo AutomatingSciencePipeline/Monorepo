fb3a8e284ab7b98c87d7db92f05e34a782ec0b0a
rhit-johnsoz2 <johnsoz2@rose-hulman.edu>
2025-04-02 16:39:54 -0400

    Will figure this out post-storm

--- apps/frontend/app/components/flows/ViewExperiment/ExperimentListing.tsx
+++ apps/frontend/app/components/flows/ViewExperiment/ExperimentListing.tsx
@@ -428,7 +428,7 @@ export const ExperimentListing = ({ projectData: projectData, onCopyExperiment, 
 							<button type="button"
 									className={`inline-flex items-center justify-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white ${
 										project.status != 'ARCHIVED' ? 'bg-yellow-600 hover:bg-yellow-700' : 'bg-gray-500 hover:bg-gray-600'
-									} focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500 xl:w-full`}
+									} focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500 xl:w-full max-w-[100px]`}
 									onClick={() => {
 										const newArchiveStatus = project.status !== 'ARCHIVED';
 										const newStatus = newArchiveStatus ? 'ARCHIVED' : 'COMPLETED';
