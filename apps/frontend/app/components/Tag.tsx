44a032b7cb94c86f889d963e658c5b0b6e3a90c7
Helena Donaldson <hmdonaldson5@gmail.com>
2025-12-26 23:56:53 +0000

    Refactored tag component

---
+++ apps/frontend/app/components/Tag.tsx
@@ -0,0 +1,16 @@
+import { XMarkIcon } from '@heroicons/react/24/solid';
+
+export function Tag(deletable, text){
+    return (
+        deletable ?
+        (<div className="cursor-pointer gap-1 flex rounded-md bg-blue-400/10 px-2 py-1 text-xs font-medium text-blue-400 inset-ring inset-ring-blue-400/30"> 
+            <span className="h-full hover:underline flex-1">{text}</span>
+            <div>
+                <XMarkIcon className="flex-none text-blue-400 cursor-pointer w-4 h-4 grid-cols-1" />
+            </div>
+        </div>) :
+        (<div>
+            <span className="inline-flex items-center rounded-md bg-blue-400/10 px-2 py-1 text-xs font-medium text-blue-400 inset-ring inset-ring-blue-400/30">{text}</span>
+        </div>)
+    );
+};
