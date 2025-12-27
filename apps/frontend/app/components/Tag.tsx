14f1860d6d01cf1cdcaeea36126f54c06ae0f800
Helena Donaldson <hmdonaldson5@gmail.com>
2025-12-27 00:02:36 +0000

    Finished refactor

--- apps/frontend/app/components/Tag.tsx
+++ apps/frontend/app/components/Tag.tsx
@@ -1,6 +1,6 @@
 import { XMarkIcon } from '@heroicons/react/24/solid';
 
-export function Tag(deletable, text){
+export function Tag({deletable, text}){
     return (
         deletable ?
         (<div className="cursor-pointer gap-1 flex rounded-md bg-blue-400/10 px-2 py-1 text-xs font-medium text-blue-400 inset-ring inset-ring-blue-400/30"> 
