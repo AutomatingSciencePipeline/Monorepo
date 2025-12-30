b57dcdb2962326e617147872b49981391da0dc7e
Helena Donaldson <hmdonaldson5@gmail.com>
2025-12-30 21:07:28 +0000

    Removed unneeded tag components and changed to solo tag for page

--- apps/frontend/app/components/ReadOnlyTag.tsx
+++
@@ -1,7 +0,0 @@
-export function ReadOnlyTag({text}: {text: string}){
-    return (
-        <div>
-            <span className="inline-flex items-center rounded-md bg-blue-400/10 px-2 py-1 text-xs font-medium text-blue-400 inset-ring inset-ring-blue-400/30">{text}</span>
-        </div>
-    );
-};
