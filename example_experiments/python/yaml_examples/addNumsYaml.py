e69ae7c8831d646eeacb472578d086291b024ed2
Helena Donaldson <hmdonaldson5@gmail.com>
2026-03-15 07:22:17 +0000

    Changed documentation from default

--- example_experiments/python/yaml_examples/addNumsYaml.py
+++ example_experiments/python/yaml_examples/addNumsYaml.py
@@ -4,23 +4,13 @@ import yaml
 
 # pylint: disable-next=pointless-string-statement
 """
-This experiment demonstrates outputting additional information to a file
-and telling the system to gather the data from that file.
-
-There are two different ways to gather additional information from an experiment
-    - Trial's Extra File: Gathers the designated file that a run of the file generates and places in a zip to be uploaded when
-        the experiment completes
-    - Trial Result: Integrates the information from a 2 line csv of headers and values that the file run generates
-        and adds them to the result csv that is being uploaded
-
-How to tell if they worked
-    - Trial's Extra File: You can download a zip file that contains the different output files specified
-    - Trial Result: The result csv downloaded has been expanded with information from the specified file
-
+This experiment demonstrates the default experiment addNums but with a yaml config file instead of an ini file. 
+The same information is being gathered and outputted, just in a different format.
 
 Example settings for a run that demonstrates this: (Any Fields not specified can be left blank or to whatever their default is)
 
 Info:
+ConfigFileFormat: yaml
 Trial Result: AddNumResult.csv
 If you want a collection of each CSV this experiment runs:
     Trial's Extra File: AddNumResult.csv
