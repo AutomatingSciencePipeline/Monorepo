530feb8700271f9a07b2846461b5fa3bb05a24ef
rhit-yuw2 <yuw2@rose-hulman.edu>
2026-03-31 00:00:24 +0000

    update Tags component

--- apps/frontend/app/components/Tag.tsx
+++ apps/frontend/app/components/Tag.tsx
@@ -4,9 +4,9 @@ export function Tag({text, onDelete}){
     return (
         (<div className="cursor-pointer gap-1 flex rounded-full border border-blue-200 bg-blue-400/10 px-2 py-1 text-xs font-medium text-blue-400 inset-ring inset-ring-blue-400/30"> 
             <span className="h-full hover:underline flex-1">{text}</span>
-            <div onClick={onDelete}>
+            {<div onClick={onDelete}>
                 <XMarkIcon className="flex-none text-blue-400 cursor-pointer w-4 h-4 grid-cols-1" />
-            </div>
+            </div>}
         </div>)
     );
 };
--- apps/frontend/app/dashboard/page.tsx
+++ apps/frontend/app/dashboard/page.tsx
@@ -1143,7 +1143,7 @@ const ExperimentList = ({ experiments, onCopyExperiment, onDeleteExperiment, sea
                 														handleMultipleFilterTag(title, newValue);
               														}}
             													/>
-            												<Tag text={title} deletable={false} />
+            												<Tag text={title} onDelete={() => {}}/>
           												</a>
         											)}
       											</MenuItem>
